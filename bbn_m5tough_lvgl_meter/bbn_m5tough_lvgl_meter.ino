78e01bdcacc257440c31686a3256273ff40ad6c4
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-16 23:45:34 +0000

    Update bbn_m5tough_lvgl_meter.ino

--- bbn_m5tough_lvgl_meter/bbn_m5tough_lvgl_meter.ino
+++ bbn_m5tough_lvgl_meter/bbn_m5tough_lvgl_meter.ino
@@ -93,46 +93,51 @@ static void set_value(void * indic, int32_t v) {
 void lv_example_meter_1(void) {
     meter = lv_meter_create(lv_scr_act());
     lv_obj_center(meter);
-    lv_obj_set_size(meter, 200, 200);
+    lv_obj_set_size(meter, 210, 210);
 
     /*Add a scale first*/
     lv_meter_scale_t * scale = lv_meter_add_scale(meter);
-    lv_meter_set_scale_ticks(meter, scale, 41, 2, 10, lv_palette_main(LV_PALETTE_GREY));
-    lv_meter_set_scale_major_ticks(meter, scale, 8, 4, 15, lv_color_black(), 10);
+    lv_meter_set_scale_ticks(meter, scale, 37, 2, 9, lv_palette_main(LV_PALETTE_GREY));
+    lv_meter_set_scale_range(meter, scale, -180, 180, 360, 90);
+
+    lv_meter_scale_t * scale2 = lv_meter_add_scale(meter);
+    lv_meter_set_scale_ticks(meter, scale2, 12, 0, 0, lv_palette_main(LV_PALETTE_GREY));               
+    lv_meter_set_scale_major_ticks(meter, scale2, 1, 3, 14, lv_color_black(), 14);    /*Every tick is major*/
+    lv_meter_set_scale_range(meter, scale2, -150, 180, 330, 120);
 
     lv_meter_indicator_t * indic;
 
-    /*Add a blue arc to the start*/
-    indic = lv_meter_add_arc(meter, scale, 3, lv_palette_main(LV_PALETTE_BLUE), 0);
-    lv_meter_set_indicator_start_value(meter, indic, 0);
-    lv_meter_set_indicator_end_value(meter, indic, 20);
+    /*Add a red arc to the start*/
+    indic = lv_meter_add_arc(meter, scale, 4, lv_palette_main(LV_PALETTE_RED), 2);
+    lv_meter_set_indicator_start_value(meter, indic, -60);
+    lv_meter_set_indicator_end_value(meter, indic, -20);
 
-    /*Make the tick lines blue at the start of the scale*/
-    indic = lv_meter_add_scale_lines(meter, scale, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_BLUE),
+    /*Make the tick lines red at the start of the scale*/
+    indic = lv_meter_add_scale_lines(meter, scale, lv_palette_main(LV_PALETTE_RED), lv_palette_main(LV_PALETTE_RED),
                                      false, 0);
-    lv_meter_set_indicator_start_value(meter, indic, 0);
-    lv_meter_set_indicator_end_value(meter, indic, 20);
+    lv_meter_set_indicator_start_value(meter, indic, -60);
+    lv_meter_set_indicator_end_value(meter, indic, -20);
 
-    /*Add a red arc to the end*/
-    indic = lv_meter_add_arc(meter, scale, 3, lv_palette_main(LV_PALETTE_RED), 0);
-    lv_meter_set_indicator_start_value(meter, indic, 80);
-    lv_meter_set_indicator_end_value(meter, indic, 100);
+    /*Add a green arc to the end*/
+    indic = lv_meter_add_arc(meter, scale, 4, lv_palette_main(LV_PALETTE_GREEN), 2);
+    lv_meter_set_indicator_start_value(meter, indic, 20);
+    lv_meter_set_indicator_end_value(meter, indic, 60);
 
-    /*Make the tick lines red at the end of the scale*/
-    indic = lv_meter_add_scale_lines(meter, scale, lv_palette_main(LV_PALETTE_RED), lv_palette_main(LV_PALETTE_RED), false,
+    /*Make the tick lines green at the end of the scale*/
+    indic = lv_meter_add_scale_lines(meter, scale, lv_palette_main(LV_PALETTE_GREEN), lv_palette_main(LV_PALETTE_GREEN), false,
                                      0);
-    lv_meter_set_indicator_start_value(meter, indic, 80);
-    lv_meter_set_indicator_end_value(meter, indic, 100);
+    lv_meter_set_indicator_start_value(meter, indic, 20);
+    lv_meter_set_indicator_end_value(meter, indic, 60);
 
     /*Add a needle line indicator*/
-    indic = lv_meter_add_needle_line(meter, scale, 4, lv_palette_main(LV_PALETTE_GREY), -10);
+    indic = lv_meter_add_needle_line(meter, scale, 6, lv_palette_main(LV_PALETTE_GREY), -10);
 
     /*Create an animation to set the value*/
     lv_anim_t a;
     lv_anim_init(&a);
     lv_anim_set_exec_cb(&a, set_value);
     lv_anim_set_var(&a, indic);
-    lv_anim_set_values(&a, 0, 100);
+    lv_anim_set_values(&a, -60, -20);
     lv_anim_set_time(&a, 2000);
     lv_anim_set_repeat_delay(&a, 100);
     lv_anim_set_playback_time(&a, 500);
