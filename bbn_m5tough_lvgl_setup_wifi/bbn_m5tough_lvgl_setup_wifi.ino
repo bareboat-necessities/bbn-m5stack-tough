c15ddf94b780658ec3e3bfd07087bbbccb7352e6
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-17 15:16:12 +0000

    Update bbn_m5tough_lvgl_setup_wifi.ino

--- bbn_m5tough_lvgl_setup_wifi/bbn_m5tough_lvgl_setup_wifi.ino
+++ bbn_m5tough_lvgl_setup_wifi/bbn_m5tough_lvgl_setup_wifi.ino
@@ -10,32 +10,30 @@
 #include <Preferences.h>
 
 // init the tft espi
-static lv_disp_draw_buf_t draw_buf;
-static lv_disp_drv_t disp_drv;   // Descriptor of a display driver
-static lv_indev_drv_t indev_drv; // Descriptor of a touch driver
+lv_disp_draw_buf_t draw_buf;
+lv_disp_drv_t disp_drv;   // Descriptor of a display driver
+lv_indev_drv_t indev_drv; // Descriptor of a touch driver
 
-M5Display *tft;
+M5Display* tft;
 
 // wifi config store.
 Preferences preferences;
 boolean settingMode;
-String wifi_ssid; // Store the name of the wireless network.
+String wifi_ssid;     // Store the name of the wireless network.
 String wifi_password; // Store the password of the wireless network.
 
-static lv_obj_t * list_wifi;
+lv_obj_t* list_wifi;
 
-static void ta_event_cb(lv_event_t * e);
-static lv_obj_t * kb;
+void ta_event_cb(lv_event_t* e);
+lv_obj_t* kb;
 
-static void setupMode() {
-    WiFi.mode(WIFI_STA);// Set WiFi to station mode and disconnect from an AP if it was previously connected.  
-    WiFi.disconnect();  // Turn off all wifi connections.  
-    delay(100); // 100 ms delay. 
-
-    int n = WiFi.scanNetworks();  // return the number of networks found. 
-    lv_list_wifi(n);
+void setupMode() {
+  WiFi.mode(WIFI_STA); // Set WiFi to station mode and disconnect from an AP if it was previously connected.  
+  WiFi.disconnect();   // Turn off all wifi connections.  
+  delay(100); // 100 ms delay. 
+  int n = WiFi.scanNetworks();  // return the number of networks found. 
+  lv_list_wifi(n);
 }
-
 
 boolean checkConnection() { // Check wifi connection. 
   int count = 0;  // count.  
@@ -51,14 +49,14 @@ boolean checkConnection() { // Check wifi connection.
   return false;
 }
 
-static void event_handler_wifi(lv_event_t * e) {
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t * obj = lv_event_get_target(e);
-    if (code == LV_EVENT_CLICKED) {
-       int n = (int) lv_event_get_user_data(e); 
-       M5.Lcd.printf("Clicked: %d %s\n", n, lv_list_get_btn_text(list_wifi, obj));
-       lv_connect_wifi_win(n);
-    }
+void event_handler_wifi(lv_event_t* e) {
+  lv_event_code_t code = lv_event_get_code(e);
+  lv_obj_t* obj = lv_event_get_target(e);
+  if (code == LV_EVENT_CLICKED) {
+    int n = (int) lv_event_get_user_data(e); 
+    M5.Lcd.printf("Clicked: %d %s\n", n, lv_list_get_btn_text(list_wifi, obj));
+    lv_connect_wifi_win(n);
+  }
 }
 
 void lv_list_wifi(int num) {
@@ -68,7 +66,7 @@ void lv_list_wifi(int num) {
   lv_obj_center(list_wifi);
 
   /*Add buttons to the list*/
-  lv_obj_t * btn;
+  lv_obj_t* btn;
 
   lv_list_add_text(list_wifi, "Wi-Fi Networks");
 
@@ -84,50 +82,50 @@ void lv_list_wifi(int num) {
   }
 }
 
-static void event_handler_connect_wifi_win(lv_event_t * e) {
-  lv_obj_t * obj = lv_event_get_target(e);
+void event_handler_connect_wifi_win(lv_event_t* e) {
+  lv_obj_t* obj = lv_event_get_target(e);
   LV_LOG_USER("Button %d clicked", (int)lv_obj_get_index(obj));
 }
 
-static void lv_win_close_event_cb(lv_event_t * e) {
-  lv_obj_del((lv_obj_t *) lv_event_get_user_data(e));
+void lv_win_close_event_cb(lv_event_t* e) {
+  lv_obj_del((lv_obj_t*) lv_event_get_user_data(e));
 }
 
 void lv_connect_wifi_win(int i) {
-  lv_obj_t * win = lv_win_create(lv_scr_act(), 40);
+  lv_obj_t* win = lv_win_create(lv_scr_act(), 40);
   lv_win_add_title(win, "Connect to Wi-Fi");
-  lv_obj_t * btn = lv_win_add_btn(win, LV_SYMBOL_CLOSE, 40);
+  lv_obj_t* btn = lv_win_add_btn(win, LV_SYMBOL_CLOSE, 40);
   lv_obj_add_event_cb(btn, lv_win_close_event_cb, LV_EVENT_CLICKED, win);
-  lv_obj_t * cont = lv_win_get_content(win);  /*Content can be added here*/
+  lv_obj_t* cont = lv_win_get_content(win);  /*Content can be added here*/
   lv_password_textarea(i, cont);
 }
 
-void lv_password_textarea(int i, lv_obj_t * cont) {
-    /*Create the password box*/
-    lv_obj_t * pwd_ta = lv_textarea_create(cont);
-    lv_textarea_set_text(pwd_ta, "");
-    lv_textarea_set_password_mode(pwd_ta, true);
-    lv_textarea_set_one_line(pwd_ta, true);
-    lv_obj_set_width(pwd_ta, lv_pct(80));
-    lv_obj_set_pos(pwd_ta, 3, 5);
-    lv_obj_add_event_cb(pwd_ta, ta_password_event_cb, LV_EVENT_ALL, NULL);
+void lv_password_textarea(int i, lv_obj_t* cont) {
+  /*Create the password box*/
+  lv_obj_t* pwd_ta = lv_textarea_create(cont);
+  lv_textarea_set_text(pwd_ta, "");
+  lv_textarea_set_password_mode(pwd_ta, true);
+  lv_textarea_set_one_line(pwd_ta, true);
+  lv_obj_set_width(pwd_ta, lv_pct(80));
+  lv_obj_set_pos(pwd_ta, 3, 5);
+  lv_obj_add_event_cb(pwd_ta, ta_password_event_cb, LV_EVENT_ALL, NULL);
 
-    /*Create a label and position it above the text box*/
-    lv_obj_t * pwd_label = lv_label_create(cont);
-    lv_obj_set_width(pwd_label, lv_pct(80));
-    lv_label_set_text(pwd_label, "Password:" /*("Password to " + WiFi.SSID(i)).c_str()*/);
-    lv_obj_align_to(pwd_label, pwd_ta, LV_ALIGN_OUT_TOP_LEFT, 0, 0);
+  /*Create a label and position it above the text box*/
+  lv_obj_t* pwd_label = lv_label_create(cont);
+  lv_obj_set_width(pwd_label, lv_pct(80));
+  lv_label_set_text(pwd_label, "Password:" /*("Password to " + WiFi.SSID(i)).c_str()*/);
+  lv_obj_align_to(pwd_label, pwd_ta, LV_ALIGN_OUT_TOP_LEFT, 0, 0);
 
-    /*Create a keyboard*/
-    kb = lv_keyboard_create(cont);
-    lv_obj_set_size(kb, LV_HOR_RES, LV_VER_RES / 2);
+  /*Create a keyboard*/
+  kb = lv_keyboard_create(cont);
+  lv_obj_set_size(kb, LV_HOR_RES, LV_VER_RES / 2);
 
-    lv_keyboard_set_textarea(kb, pwd_ta); /*Focus it on one of the text areas to start*/
+  lv_keyboard_set_textarea(kb, pwd_ta); /*Focus it on one of the text areas to start*/
 }
 
-static void ta_password_event_cb(lv_event_t * e) {
+void ta_password_event_cb(lv_event_t* e) {
   lv_event_code_t code = lv_event_get_code(e);
-  lv_obj_t * ta = lv_event_get_target(e);
+  lv_obj_t* ta = lv_event_get_target(e);
   if (code == LV_EVENT_CLICKED || code == LV_EVENT_FOCUSED) {
     /*Focus on the clicked text area*/
     if (kb != NULL) lv_keyboard_set_textarea(kb, ta);
@@ -137,25 +135,25 @@ static void ta_password_event_cb(lv_event_t * e) {
   }
 }
 
-static void event_msgbox_cb(lv_event_t * e)
+void event_msgbox_cb(lv_event_t* e)
 {
-    lv_obj_t * obj = lv_event_get_current_target(e);
-    lv_msgbox_close(obj);
+  lv_obj_t* obj = lv_event_get_current_target(e);
+  lv_msgbox_close(obj);
 }
 
-static void lv_msgbox(const char* txt)
+void lv_msgbox(const char* txt)
 {
-    static const char * btns[] ={"Close", ""};
-    lv_obj_t * mbox = lv_msgbox_create(NULL, "", "Password submitted", btns, true);
-    lv_obj_add_event_cb(mbox, event_msgbox_cb, LV_EVENT_VALUE_CHANGED, NULL);
-    lv_obj_center(mbox);
+  const char* btns[] ={"Close", ""};
+  lv_obj_t* mbox = lv_msgbox_create(NULL, "", "Password submitted", btns, true);
+  lv_obj_add_event_cb(mbox, event_msgbox_cb, LV_EVENT_VALUE_CHANGED, NULL);
+  lv_obj_center(mbox);
 }
 
 void tft_lv_initialization() {
   M5.begin();
   lv_init();
 
-  static lv_color_t buf1[(LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10];  // Declare a buffer for 1/10 screen siz
+  lv_color_t buf1[(LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10];  // Declare a buffer for 1/10 screen siz
 
   // Initialize `disp_buf` display buffer with the buffer(s).
   lv_disp_draw_buf_init(&draw_buf, buf1, NULL, (LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10);
@@ -163,7 +161,7 @@ void tft_lv_initialization() {
 }
 
 // Display flushing
-void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t* area, lv_color_t* color_p) {
   uint32_t w = (area->x2 - area->x1 + 1);
   uint32_t h = (area->y2 - area->y1 + 1);
 
@@ -187,7 +185,7 @@ void init_disp_driver() {
   lv_disp_set_bg_color(NULL, lv_color_hex3(0x000));  // Set default background color to black
 }
 
-void my_touchpad_read(lv_indev_drv_t * drv, lv_indev_data_t * data) {
+void my_touchpad_read(lv_indev_drv_t* drv, lv_indev_data_t* data) {
   TouchPoint_t pos = M5.Touch.getPressPoint();
   bool touched = ( pos.x == -1 ) ? false : true;  
   if (!touched) {    
@@ -222,7 +220,7 @@ void setup() {
   tft_lv_initialization();
   init_disp_driver();
   init_touch_driver();
-  preferences.begin("wifi-config");
+  preferences.begin("wifi-config2");
   delay(10);
   if (restoreConfig()) {  // Check if wifi configuration information has been stored.  
     if (checkConnection()) {  // Check wifi connection.  
@@ -239,7 +237,6 @@ void loop() {
   lv_task_handler();  
   lv_tick_inc(1);
   if (settingMode) {
+    lv_tick_inc(1);
   }
 }
-
-
