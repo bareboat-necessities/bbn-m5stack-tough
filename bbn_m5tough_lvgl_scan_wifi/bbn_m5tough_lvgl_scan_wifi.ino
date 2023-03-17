5e1ea646c5e2a70acb51165236c2079d9d9e82cb
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-17 06:47:39 +0000

    Update bbn_m5tough_lvgl_scan_wifi.ino

--- bbn_m5tough_lvgl_scan_wifi/bbn_m5tough_lvgl_scan_wifi.ino
+++ bbn_m5tough_lvgl_scan_wifi/bbn_m5tough_lvgl_scan_wifi.ino
@@ -67,12 +67,18 @@ static void event_handler_connect_wifi_win(lv_event_t * e) {
   LV_LOG_USER("Button %d clicked", (int)lv_obj_get_index(obj));
 }
 
+static void lv_win_close_event_cb(lv_event_t * e) {
+  lv_obj_t * content = lv_obj_get_child((lv_obj_t *) lv_event_get_user_data(e), 1);
+  lv_obj_del(content);
+  lv_obj_del(lv_obj_get_child((lv_obj_t *) lv_event_get_user_data(e), 0));                                                                                                                                                                                                                                      
+  lv_obj_del((lv_obj_t *) lv_event_get_user_data(e));
+}
+
 void lv_connect_wifi_win(int i) {
   lv_obj_t * win = lv_win_create(lv_scr_act(), 40);
   lv_win_add_title(win, "Connect to Wi-Fi");
-  lv_obj_t * btn;
-  btn = lv_win_add_btn(win, LV_SYMBOL_CLOSE, 60);
-  lv_obj_add_event_cb(btn, event_handler_connect_wifi_win, LV_EVENT_CLICKED, NULL);
+  lv_obj_t * btn = lv_win_add_btn(win, LV_SYMBOL_CLOSE, 40);
+  lv_obj_add_event_cb(btn, lv_win_close_event_cb, LV_EVENT_CLICKED, win);
   lv_obj_t * cont = lv_win_get_content(win);  /*Content can be added here*/
   lv_password_textarea(i, cont);
 }
@@ -83,17 +89,17 @@ void lv_password_textarea(int i, lv_obj_t * cont) {
     lv_textarea_set_text(pwd_ta, "");
     lv_textarea_set_password_mode(pwd_ta, true);
     lv_textarea_set_one_line(pwd_ta, true);
-    lv_obj_set_width(pwd_ta, lv_pct(100));
-    lv_obj_set_pos(pwd_ta, 5, 20);
+    lv_obj_set_width(pwd_ta, lv_pct(90));
+    lv_obj_set_pos(pwd_ta, 4, 18);
     lv_obj_add_event_cb(pwd_ta, ta_password_event_cb, LV_EVENT_ALL, NULL);
 
     /*Create a label and position it above the text box*/
-    lv_obj_t * pwd_label = lv_label_create(lv_scr_act());
-    lv_label_set_text(pwd_label, ("Password to " + WiFi.SSID(i)).c_str());
+    lv_obj_t * pwd_label = lv_label_create(cont);
+    lv_label_set_text(pwd_label, ("Password to " + WiFi.SSID(i)).substring(0, 25).c_str());
     lv_obj_align_to(pwd_label, pwd_ta, LV_ALIGN_OUT_TOP_LEFT, 0, 0);
 
     /*Create a keyboard*/
-    kb = lv_keyboard_create(lv_scr_act());
+    kb = lv_keyboard_create(cont);
     lv_obj_set_size(kb,  LV_HOR_RES, LV_VER_RES / 2);
 
     lv_keyboard_set_textarea(kb, pwd_ta); /*Focus it on one of the text areas to start*/
@@ -116,10 +122,9 @@ void tft_lv_initialization() {
   lv_init();
 
   static lv_color_t buf1[(LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10];  // Declare a buffer for 1/10 screen siz
-  static lv_color_t buf2[(LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10];  // second buffer is optionnal
 
   // Initialize `disp_buf` display buffer with the buffer(s).
-  lv_disp_draw_buf_init(&draw_buf, buf1, buf2, (LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10);
+  lv_disp_draw_buf_init(&draw_buf, buf1, NULL, (LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10);
   tft = &M5.Lcd;
 }
 
