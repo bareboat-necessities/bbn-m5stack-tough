23625da07a645dc91145b308f7f3c02f988c6817
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-22 10:40:01 -0400

    Create bbn_m5tough_sensesp.ino

---
+++ bbn_m5tough_sensesp/bbn_m5tough_sensesp.ino
@@ -0,0 +1,150 @@
+/**
+ * @brief Example and test program for using FreeRTOS tasks.
+ *
+ */
+
+/*
+  Required libraries
+
+   mairas/ReactESP@^2.0.0
+   me-no-dev/ESPAsyncTCP by dvarrel
+   me-no-dev/ESP Async Webserver by dvarrel https://github.com/me-no-dev/ESPAsyncWebserver  https://github.com/me-no-dev/ESPAsyncWebServer/archive/refs/heads/master.zip
+   alanswx/ESPAsyncWifiManager   https://github.com/alanswx/ESPAsyncWifiManager 
+   bblanchon/ArduinoJson
+   links2004/WebSockets https://github.com/Links2004/arduinoWebSockets
+   pfeerick/elapsedMillis
+   https://github.com/JoaoLopesF/RemoteDebug.git
+   
+   add 
+   
+   #define A0 GPIO_NUM_34
+   
+   into ~/Arduino/libraries/SensESP/src/sensesp/sensors/analog_input.h
+*/
+#include <M5Tough.h>
+#undef min(a, b)
+
+namespace sensesp {
+  const uint8_t A0 = GPIO_NUM_34;
+}
+
+#include <sensesp.h>
+#include <sensesp_app.h>
+
+//#include "sensesp/controllers/system_status_controller.h"
+//#define A0 GPIO_NUM_34
+
+
+
+#include "sensesp/sensors/analog_input.h"
+
+#include "sensesp/net/discovery.h"
+#include "sensesp/net/http_server.h"
+#include "sensesp/net/networking.h"
+#include "sensesp/net/ws_client.h"
+//#include "sensesp/sensors/digital_input.h"
+#include "sensesp/signalk/signalk_delta_queue.h"
+#include "sensesp/signalk/signalk_output.h"
+#include "sensesp/signalk/signalk_value_listener.h"
+#include "sensesp/system/lambda_consumer.h"
+//#include "sensesp/system/system_status_led.h"
+#include "sensesp_minimal_app_builder.h"
+
+
+using namespace sensesp;
+
+
+const int kTestOutputPin = GPIO_NUM_18;
+// repetition interval in ms; corresponds to 1000/(2*5)=100 Hz
+const int kTestOutputInterval = 410;
+
+const uint8_t kDigitalInputPin = 15;
+
+reactesp::ReactESP app;
+
+/*
+void ToggleTestOutputPin(void *parameter) {
+  while (true) {
+    digitalWrite(kTestOutputPin, !digitalRead(kTestOutputPin));
+    delay(kTestOutputInterval);
+  }
+}
+*/
+
+// The setup function performs one-time application initialization.
+void setup() {
+   M5.begin();
+// Some initialization boilerplate when in debug mode...
+#ifndef SERIAL_DEBUG_DISABLED
+  SetupSerialDebug(115200);
+#endif
+
+  SensESPMinimalAppBuilder builder;
+  SensESPMinimalApp *sensesp_app = builder.set_hostname("sensesp")->get_app();
+
+  auto *networking = new Networking(
+    "/system/net", "", "", SensESPBaseApp::get_hostname(), "thisisfine");
+  auto *http_server = new HTTPServer();
+
+  // create the SK delta object
+  auto sk_delta_queue_ = new SKDeltaQueue();
+
+  // create the websocket client
+  auto ws_client_ = new WSClient("/system/sk", sk_delta_queue_, "", 0);
+
+  ws_client_->connect_to(new LambdaConsumer<WSConnectionState>(
+    [](WSConnectionState input) {
+      debugD("WSConnectionState: %d", input);
+    }));
+
+  // create the MDNS discovery object
+  auto mdns_discovery_ = new MDNSDiscovery();
+
+  // create a system status controller and a led blinker
+
+  //auto *system_status_controller = new SystemStatusController();
+  //auto *system_status_led = new SystemStatusLed(LED_PIN);
+
+  //system_status_controller->connect_to(system_status_led);
+  //ws_client_->get_delta_count_producer().connect_to(system_status_led);
+
+  // create a new task for toggling the output pin
+
+  //pinMode(kTestOutputPin, OUTPUT);
+  //xTaskCreate(ToggleTestOutputPin, "toggler", 2048, NULL, 1, NULL);
+
+  // listen to the changes on the digital input pin
+
+  //auto digin = new DigitalInputChange(kDigitalInputPin, INPUT_PULLUP, CHANGE);
+
+  //digin->connect_to(new LambdaConsumer<bool>([](bool input) {
+  //  debugD("(%d ms) Digital input changed to %d", millis(), input);
+  //}));
+
+  // connect digin to the SK delta queue
+
+  //const char *sk_path = "environment.bool.pin15";
+ // digin->connect_to(new SKOutputFloat(sk_path, ""));
+
+  // create a new SKListener for navigation.headingMagnetic
+
+  auto hdg = new SKValueListener<float>("navigation.headingMagnetic");
+  hdg->connect_to(new LambdaConsumer<float>([](float input) {
+    debugD("Heading: %f", input);
+  }));
+
+  // print out free heap
+  app.onRepeat(2000, []() {
+    debugD("Free heap: %d", ESP.getFreeHeap());
+  });
+
+  // Start the SensESP application running
+  sensesp_app->start();
+}
+
+// The loop function is called in an endless loop during program execution.
+// It simply calls `app.tick()` which will then execute all reactions as needed.
+void loop() {
+  M5.update();
+  app.tick();
+}
