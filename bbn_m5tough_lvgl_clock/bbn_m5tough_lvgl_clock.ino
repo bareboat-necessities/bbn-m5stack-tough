4b9bda4584e1fc84196c20acefa75c7e2a0dec95
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-23 23:18:17 -0400

    Update bbn_m5tough_lvgl_clock.ino

--- bbn_m5tough_lvgl_clock/bbn_m5tough_lvgl_clock.ino
+++ bbn_m5tough_lvgl_clock/bbn_m5tough_lvgl_clock.ino
@@ -101,9 +101,9 @@ void lv_clock_display(lv_obj_t *parent) {
 
   /*Create a scale for the minutes*/
   /*61 ticks in a 360 degrees range (the last and the first line overlaps)*/
-  lv_meter_scale_t *scale_min = lv_meter_add_scale(clock_display);
-  lv_meter_set_scale_ticks(clock_display, scale_min, 61, 1, 10, lv_palette_main(LV_PALETTE_GREY));
-  lv_meter_set_scale_range(clock_display, scale_min, 0, 60, 360, 270);
+  lv_meter_scale_t *scale = lv_meter_add_scale(clock_display);
+  lv_meter_set_scale_ticks(clock_display, scale, 61, 1, 10, lv_palette_main(LV_PALETTE_GREY));
+  lv_meter_set_scale_range(clock_display, scale, 0, 60, 360, 270);
 
   /*Create another scale for the hours. It's only visual and contains only major ticks*/
   lv_meter_scale_t *scale_hour = lv_meter_add_scale(clock_display);
@@ -111,9 +111,9 @@ void lv_clock_display(lv_obj_t *parent) {
   lv_meter_set_scale_major_ticks(clock_display, scale_hour, 1, 2, 20, lv_palette_main(LV_PALETTE_GREY), 10); /*Every tick is major*/
   lv_meter_set_scale_range(clock_display, scale_hour, 1, 12, 330, 300);                                      /*[1..12] values in an almost full circle*/
 
-  indic_sec = lv_meter_add_needle_line(clock_display, scale_min, 2, lv_palette_main(LV_PALETTE_GREY), -10);
-  indic_min = lv_meter_add_needle_line(clock_display, scale_min, 4, lv_palette_main(LV_PALETTE_GREY), -20);
-  indic_hour = lv_meter_add_needle_line(clock_display, scale_min, 6, lv_palette_main(LV_PALETTE_GREY), -42);
+  indic_sec = lv_meter_add_needle_line(clock_display, scale, 2, lv_palette_main(LV_PALETTE_GREY), -10);
+  indic_min = lv_meter_add_needle_line(clock_display, scale, 4, lv_palette_main(LV_PALETTE_GREEN), -20);
+  indic_hour = lv_meter_add_needle_line(clock_display, scale, 7, lv_palette_main(LV_PALETTE_RED), -42);
 }
 
 Gesture swipeDown("swipe down", 80, DIR_DOWN, 40);
