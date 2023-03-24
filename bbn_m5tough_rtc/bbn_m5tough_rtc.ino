1fdb4ff7047bdb7604e6d6be6c66b9caa1d39fdf
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-24 19:55:33 -0400

    Update bbn_m5tough_rtc.ino

--- bbn_m5tough_rtc/bbn_m5tough_rtc.ino
+++ bbn_m5tough_rtc/bbn_m5tough_rtc.ino
@@ -1,15 +1,15 @@
 #include <M5Tough.h>
 
 RTC_TimeTypeDef RTCtime;
-RTC_DateTypeDef RTCDate;
+RTC_DateTypeDef RTCdate;
 
 char timeStrbuff[64];
 
 void flushTime() {
   M5.Rtc.GetTime(&RTCtime);  //Gets the time from the real-time clock.
-  M5.Rtc.GetDate(&RTCDate);
+  M5.Rtc.GetDate(&RTCdate);
   sprintf(timeStrbuff, "%d/%02d/%02d %02d:%02d:%02d",  //Stores real-time time and date data to timeStrbuff.
-          RTCDate.Year, RTCDate.Month, RTCDate.Date,
+          RTCdate.Year, RTCdate.Month, RTCdate.Date,
           RTCtime.Hours, RTCtime.Minutes, RTCtime.Seconds);
   M5.lcd.setCursor(10, 100);  //Move the cursor position to (x,y).
   M5.Lcd.println(timeStrbuff);
@@ -21,10 +21,10 @@ void setupTime() {
   //RTCtime.Seconds = 33;
   //M5.Rtc.SetTime(&RTCtime);  //and writes the set time to the real time clock.
 
-  //RTCDate.Year = 2023;  //Set the date.
-  //RTCDate.Month = 3;
-  //RTCDate.Date = 24;
-  //M5.Rtc.SetDate(&RTCDate);
+  //RTCdate.Year = 2023;  //Set the date.
+  //RTCdate.Month = 3;
+  //RTCdate.Date = 24;
+  //M5.Rtc.SetDate(&RTCdate);
 }
 
 /* 
