e54f405454d315cc0c9053c90d9a74cbd6eb3ccb
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-23 21:22:11 -0400

    Update bbn_m5tough_rtc_wakeup.ino

--- bbn_m5tough_rtc_wakeup/bbn_m5tough_rtc_wakeup.ino
+++ bbn_m5tough_rtc_wakeup/bbn_m5tough_rtc_wakeup.ino
@@ -1,1 +1,24 @@
+#include <M5Tough.h>
 
+//RTC_TimeTypeDef RTCtime;
+RTC_TimeTypeDef RTCtime_Now;
+
+char timeStrBuf[64];
+
+void setup() {
+  M5.begin();
+  M5.Lcd.println("I will wait for 10 seconds before a shutdown.");
+  M5.Lcd.println("Then I will wake up after 5 seconds.");
+  delay(10000);
+  M5.shutdown(5);
+  //M5.shutdown(RTCtime); //Turn off the power and wake up at the specified time.
+}
+
+void loop() {
+  M5.update();
+  M5.Lcd.setCursor(0, 140);
+  M5.Rtc.GetTime(&RTCtime_Now);                          //Gets the current time.
+  sprintf(timeStrBuf, "RTC Time Now is %02d:%02d:%02d",  //Stores real-time time data to timeStrBuf.
+          RTCtime_Now.Hours, RTCtime_Now.Minutes, RTCtime_Now.Seconds);
+  M5.Lcd.println(timeStrBuf);  //Screen printing output timeStrBuf.
+}
