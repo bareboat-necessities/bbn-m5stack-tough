259d41508ba6716c253346d1706b842217ac37b1
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-24 20:12:40 -0400

    Update bbn_m5tough_time_ntp.ino

--- bbn_m5tough_time_ntp/bbn_m5tough_time_ntp.ino
+++ bbn_m5tough_time_ntp/bbn_m5tough_time_ntp.ino
@@ -11,6 +11,9 @@ const char* ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 0;
 const int daylightOffset_sec = 3600;
 
+RTC_TimeTypeDef RTCtime;
+RTC_DateTypeDef RTCdate;
+
 void printLocalTime() {
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo)) {  //Return 1 when the time is successfully obtained.
@@ -36,8 +39,25 @@ void setup() {
   M5.Lcd.println("\nCONNECTED!");
   delay(2000);
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);  //init and get the time.
+  setupTime();
   printLocalTime();
   delay(20);
+}
+
+void setupTime() {
+  struct tm timeinfo;
+  if (getLocalTime(&timeinfo)) { 
+    RTCtime.Hours = timeinfo.tm_hour;
+    RTCtime.Minutes = timeinfo.tm_min;
+    RTCtime.Seconds = timeinfo.tm_sec;
+    //RTCtime.Daylight = timeinfo.tm_isdst;
+    M5.Rtc.SetTime(&RTCtime);  // writes the  time to the (RTC) real time clock.
+
+    RTCdate.Year = timeinfo.tm_year; 
+    RTCdate.Month = timeinfo.tm_mon;
+    RTCdate.Date = timeinfo.tm_mday;
+    M5.Rtc.SetDate(&RTCdate);
+  }
 }
 
 void loop() {
