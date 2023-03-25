41de17e5de5b6823c62b89623da7896edce9af67
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-24 20:41:26 -0400

    Update bbn_m5tough_lvgl_scan_wifi.ino

--- bbn_m5tough_lvgl_scan_wifi/bbn_m5tough_lvgl_scan_wifi.ino
+++ bbn_m5tough_lvgl_scan_wifi/bbn_m5tough_lvgl_scan_wifi.ino
@@ -15,8 +15,6 @@ static lv_indev_drv_t indev_drv;  // Descriptor of a touch driver
 
 M5Display *tft;
 
-static lv_obj_t *kb;
-
 static void btnScanWifi_event(lv_event_t *event) {
   WiFi.mode(WIFI_STA);  // Set WiFi to station mode and disconnect from an AP if it was previously connected.
   WiFi.disconnect();    // Turn off all wifi connections.
@@ -27,16 +25,6 @@ static void btnScanWifi_event(lv_event_t *event) {
 }
 
 static lv_obj_t *list_wifi;
-
-static void event_handler_wifi(lv_event_t *e) {
-  lv_event_code_t code = lv_event_get_code(e);
-  lv_obj_t *obj = lv_event_get_target(e);
-  if (code == LV_EVENT_CLICKED) {
-    int n = (int)lv_event_get_user_data(e);
-    M5.Lcd.printf("Clicked: %d %s\n", n, lv_list_get_btn_text(list_wifi, obj));
-    lv_connect_wifi_win(n);
-  }
-}
 
 void lv_list_wifi(int num) {
   /*Create a list*/
@@ -51,7 +39,6 @@ void lv_list_wifi(int num) {
 
   for (int i = 0; i < num; ++i) {
     btn = lv_list_add_btn(list_wifi, LV_SYMBOL_WIFI, (((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? String(LV_SYMBOL_EYE_OPEN "  ") : String()) + WiFi.SSID(i)).c_str());
-    lv_obj_add_event_cb(btn, event_handler_wifi, LV_EVENT_CLICKED, (void *)i);
     delay(10);
   }
 }
@@ -65,48 +52,6 @@ static void lv_win_close_event_cb(lv_event_t *e) {
   lv_obj_del((lv_obj_t *)lv_event_get_user_data(e));
 }
 
-void lv_connect_wifi_win(int i) {
-  lv_obj_t *win = lv_win_create(lv_scr_act(), 40);
-  lv_win_add_title(win, "Connect to Wi-Fi");
-  lv_obj_t *btn = lv_win_add_btn(win, LV_SYMBOL_CLOSE, 40);
-  lv_obj_add_event_cb(btn, lv_win_close_event_cb, LV_EVENT_CLICKED, win);
-  lv_obj_t *cont = lv_win_get_content(win); /*Content can be added here*/
-  lv_password_textarea(i, cont);
-}
-
-void lv_password_textarea(int i, lv_obj_t *cont) {
-  /*Create the password box*/
-  lv_obj_t *pwd_ta = lv_textarea_create(cont);
-  lv_textarea_set_text(pwd_ta, "");
-  lv_textarea_set_password_mode(pwd_ta, true);
-  lv_textarea_set_one_line(pwd_ta, true);
-  lv_obj_set_width(pwd_ta, lv_pct(80));
-  lv_obj_set_pos(pwd_ta, 4, 6);
-  lv_obj_add_event_cb(pwd_ta, ta_password_event_cb, LV_EVENT_ALL, NULL);
-
-  /*Create a label and position it above the text box*/
-  lv_obj_t *pwd_label = lv_label_create(cont);
-  lv_obj_set_width(pwd_label, lv_pct(80));
-  lv_label_set_text(pwd_label, "Password:" /*("Password to " + WiFi.SSID(i)).c_str()*/);
-  lv_obj_align_to(pwd_label, pwd_ta, LV_ALIGN_OUT_TOP_LEFT, 0, 0);
-
-  /*Create a keyboard*/
-  kb = lv_keyboard_create(cont);
-  lv_obj_set_size(kb, LV_HOR_RES, LV_VER_RES / 2);
-
-  lv_keyboard_set_textarea(kb, pwd_ta); /*Focus it on one of the text areas to start*/
-}
-
-static void ta_password_event_cb(lv_event_t *e) {
-  lv_event_code_t code = lv_event_get_code(e);
-  lv_obj_t *ta = lv_event_get_target(e);
-  if (code == LV_EVENT_CLICKED || code == LV_EVENT_FOCUSED) {
-    /*Focus on the clicked text area*/
-    if (kb != NULL) lv_keyboard_set_textarea(kb, ta);
-  } else if (code == LV_EVENT_READY) {
-    LV_LOG_USER("Ready, current text: %s", lv_textarea_get_text(ta));
-  }
-}
 
 void tft_lv_initialization() {
   M5.begin();
