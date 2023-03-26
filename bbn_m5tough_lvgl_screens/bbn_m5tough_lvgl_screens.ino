b81b152009839301cbbf78b0e8e75ffd56bf93ff
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-26 14:15:46 -0400

    Update bbn_m5tough_lvgl_screens.ino

--- bbn_m5tough_lvgl_screens/bbn_m5tough_lvgl_screens.ino
+++ bbn_m5tough_lvgl_screens/bbn_m5tough_lvgl_screens.ino
@@ -100,14 +100,14 @@ void setup() {
   lv_obj_align(btn1, LV_ALIGN_CENTER, 0, 0);
   lv_label_set_text(label1, "Reboot");
   lv_obj_center(label1);
-  lv_obj_add_event_cb(btn1, btnReboot_event, LV_EVENT_PRESSED, NULL);
+  lv_obj_add_event_cb(btn1, btnReboot_event, LV_EVENT_LONG_PRESSED, NULL);
 
   lv_obj_t *btn2 = lv_btn_create(page2);
   lv_obj_t *label2 = lv_label_create(btn2);
   lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 0);
   lv_label_set_text(label2, "Power Off");
   lv_obj_center(label2);
-  lv_obj_add_event_cb(btn2, btnPowerOff_event, LV_EVENT_PRESSED, NULL);
+  lv_obj_add_event_cb(btn2, btnPowerOff_event, LV_EVENT_LONG_PRESSED, NULL);
 }
 
 Gesture swipe("swipe", 80, DIR_ANY, 40);
