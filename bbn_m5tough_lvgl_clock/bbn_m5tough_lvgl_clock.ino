6e17bd06b5dcbf91b6742202c78dec82c69e8f0c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-26 14:35:35 -0400

    Update bbn_m5tough_lvgl_clock.ino

--- bbn_m5tough_lvgl_clock/bbn_m5tough_lvgl_clock.ino
+++ bbn_m5tough_lvgl_clock/bbn_m5tough_lvgl_clock.ino
@@ -1,11 +1,11 @@
-#define LV_HOR_RES_MAX 320
-#define LV_VER_RES_MAX 240
-
 #include <M5Tough.h>
 #include <Arduino.h>
 #include <lvgl.h>
 #include <Wire.h>
 #include <SPI.h>
+
+#define LV_HOR_RES_MAX 320
+#define LV_VER_RES_MAX 240
 
 // init the tft espi
 static lv_disp_draw_buf_t draw_buf;
@@ -14,12 +14,20 @@ static lv_indev_drv_t indev_drv;  // Descriptor of a touch driver
 
 M5Display *tft;
 
+static void btnPowerOff_event(lv_event_t *event) {
+  M5.Axp.PowerOff();
+}
+
+static void btnReboot_event(lv_event_t *event) {
+  ESP.restart();
+}
+
 void tft_lv_initialization() {
   M5.begin();
   lv_init();
 
-  static lv_color_t buf1[(LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10];  // Declare a buffer for 1/10 screen siz
-  static lv_color_t buf2[(LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10];  // second buffer is optionnal
+  static lv_color_t *buf1 = (lv_color_t *)heap_caps_malloc((LV_HOR_RES_MAX * LV_VER_RES_MAX * sizeof(lv_color_t)) / 10, MALLOC_CAP_8BIT | MALLOC_CAP_SPIRAM);
+  static lv_color_t *buf2 = (lv_color_t *)heap_caps_malloc((LV_HOR_RES_MAX * LV_VER_RES_MAX * sizeof(lv_color_t)) / 10, MALLOC_CAP_8BIT | MALLOC_CAP_SPIRAM);
 
   // Initialize `disp_buf` display buffer with the buffer(s).
   lv_disp_draw_buf_init(&draw_buf, buf1, buf2, (LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10);
@@ -71,15 +79,10 @@ void init_touch_driver() {
   lv_indev_t *my_indev = lv_indev_drv_register(&indev_drv);  // register
 }
 
-static int theme = 1;
+lv_obj_t *pages[2];
+int page = 0;
 
-void setup() {
-  tft_lv_initialization();
-  init_disp_driver();
-  init_touch_driver();
-  lv_theme_default_init(NULL, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), theme, LV_FONT_DEFAULT);
-  lv_clock_display(lv_scr_act());
-}
+int theme = 1;
 
 static lv_obj_t *clock_display;
 lv_meter_indicator_t *indic_sec;
@@ -90,6 +93,38 @@ RTC_TimeTypeDef RTCtime;
 static void set_value(void *indic, int32_t v) {
   lv_meter_set_indicator_value(clock_display, (lv_meter_indicator_t *)indic, v);
 }
+
+void setup() {
+  tft_lv_initialization();
+  init_disp_driver();
+  init_touch_driver();
+  lv_theme_default_init(NULL, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), theme, LV_FONT_DEFAULT);
+
+  lv_obj_t *page1 = lv_obj_create(NULL); // Creates a Screen object
+  lv_obj_t *page2 = lv_obj_create(NULL); // Creates another Screen object
+
+  pages[0] = page1;
+  pages[1] = page2;
+
+  lv_clock_display(page1);
+
+  lv_scr_load(pages[page]);
+
+  lv_obj_t *btn1 = lv_btn_create(page2);
+  lv_obj_t *label1 = lv_label_create(btn1);
+  lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -30);
+  lv_label_set_text(label1, "Reboot");
+  lv_obj_center(label1);
+  lv_obj_add_event_cb(btn1, btnReboot_event, LV_EVENT_LONG_PRESSED, NULL);
+  lv_obj_t *btn2 = lv_btn_create(page2);
+  lv_obj_t *label2 = lv_label_create(btn2);
+  lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 30);
+  lv_label_set_text(label2, "Power Off");
+  lv_obj_center(label2);
+  lv_obj_add_event_cb(btn2, btnPowerOff_event, LV_EVENT_LONG_PRESSED, NULL);
+}
+
+Gesture swipe("swipe", 80, DIR_ANY, 40);
 
 /**
  * A clock display 
@@ -116,18 +151,17 @@ void lv_clock_display(lv_obj_t *parent) {
   indic_hour = lv_meter_add_needle_line(clock_display, scale, 7, lv_palette_main(LV_PALETTE_RED), -42);
 }
 
-Gesture swipeDown("swipe down", 80, DIR_DOWN, 40);
-
 void loop() {
   M5.update();
-  if (swipeDown.wasDetected()) {
-    theme = 1 - theme;
-    lv_theme_default_init(NULL, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), theme, LV_FONT_DEFAULT);
-  }
   lv_task_handler();
-  lv_tick_inc(5);
+  if (swipe.wasDetected()) {
+    page = 1 - page;
+    lv_scr_load(pages[page]);
+  }
   M5.Rtc.GetTime(&RTCtime);
   set_value(indic_hour, RTCtime.Hours);
   set_value(indic_min, RTCtime.Minutes);
   set_value(indic_sec, RTCtime.Seconds);
+  lv_tick_inc(1);
+  delay(3);
 }
