93f7ad8deb517e6036a22e55e4bfb5262935ff5e
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-29 12:18:15 -0400

    Update ui_gestures.h

--- bbn_m5tough_active_boat/ui_gestures.h
+++ bbn_m5tough_active_boat/ui_gestures.h
@@ -1,1 +1,27 @@
+#ifndef UI_GESTURES_H
+#define UI_GESTURES_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+Gesture swipeDown("swipe down", 80, DIR_DOWN, 40);
+Gesture swipeUp("swipe up", 80, DIR_UP, 40);
+
+Gesture swipeLeft("swipe left", 80, DIR_LEFT, 40);
+Gesture swipeRight("swipe right", 80, DIR_RIGHT, 40);
+
+bool swipe_horiz_detected() {
+  return swipeRight.wasDetected() || swipeLeft.wasDetected();
+}
+
+bool swipe_vert_detected() {
+  return swipeDown.wasDetected() || swipeUp.wasDetected();
+}
+  
+  #ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
