2af9672085ebec3eaa7d62df0a85363904cf9b90
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-29 17:09:00 -0400

    Update bbn_m5tough_active_boat.ino

--- bbn_m5tough_active_boat/bbn_m5tough_active_boat.ino
+++ bbn_m5tough_active_boat/bbn_m5tough_active_boat.ino
@@ -12,31 +12,35 @@
 #include "ui_theme.h"
 #include "ui_screens.h"
 
-lv_updatable_screen_t scr1, scr2;
-
-lv_updatable_screen_t* screens[] = {
-  &scr1, &scr2
-};
-
-int page = 0;
+lv_updatable_screen_t powerOffScreen;
 
 static void btnPowerOff_event(lv_event_t *event) {
   M5.Axp.PowerOff();
 }
+
+void init_powerOffScreen() {
+  powerOffScreen.screen = lv_obj_create(NULL); // Creates a Screen object
+  lv_obj_t *btn = lv_btn_create(powerOffScreen.screen);
+  lv_obj_t *label = lv_label_create(btn);
+  lv_obj_align(btn, LV_ALIGN_CENTER, 0, 0);
+  lv_label_set_text(label, "Power Off");
+  lv_obj_center(label);
+  lv_obj_add_event_cb(btn, btnPowerOff_event, LV_EVENT_PRESSED, NULL);
+}
+
+lv_updatable_screen_t* screens[] = {
+  &powerOffScreen
+};
+
+int page = 0;
 
 void setup() {
   tft_lv_initialization();
   init_disp_driver();
   init_touch_driver();
   init_theme();
-  
-  scr1.screen = lv_obj_create(NULL); // Creates a Screen object
-  lv_obj_t *btn = lv_btn_create(scr1.screen);
-  lv_obj_t *label = lv_label_create(btn);
-  lv_obj_align(btn, LV_ALIGN_CENTER, 0, 0);
-  lv_label_set_text(label, "Power Off");
-  lv_obj_center(label);
-  lv_obj_add_event_cb(btn, btnPowerOff_event, LV_EVENT_PRESSED, NULL);
+
+  init_powerOffScreen();    
 
   lv_scr_load(screens[page]->screen);
 }
@@ -49,5 +53,5 @@ void loop() {
   if (swipe_vert_detected()) toggle_ui_theme();
   //if (swipe_horiz_detected()) next_screen();
 
-  update_screen(scr1);
+  update_screen(powerOffScreen);
 }
