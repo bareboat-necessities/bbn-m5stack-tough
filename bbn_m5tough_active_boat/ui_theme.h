4a447d4c445cfc50444a7fa4f2012b8a765cec42
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-29 14:53:53 -0400

    Update ui_theme.h

--- bbn_m5tough_active_boat/ui_theme.h
+++ bbn_m5tough_active_boat/ui_theme.h
@@ -1,1 +1,19 @@
+#ifndef UI_THEME_H
+#define UI_THEME_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  int theme = 0;
+
+  void toggle_ui_theme() {
+    theme = 1 - theme;
+    lv_theme_default_init(NULL, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), theme, LV_FONT_DEFAULT);
+  }
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
