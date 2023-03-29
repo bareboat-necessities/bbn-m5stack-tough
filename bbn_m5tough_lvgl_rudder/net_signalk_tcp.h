a2788223eb8ffe6d1357abb1022a3d6c53730f3b
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-28 21:10:36 -0400

    Update net_signalk_tcp.h

--- bbn_m5tough_lvgl_rudder/net_signalk_tcp.h
+++ bbn_m5tough_lvgl_rudder/net_signalk_tcp.h
@@ -72,11 +72,25 @@ extern "C" {
     client.flush();
   }
 
+  void setup_reconnect(WiFiClient& client, const char* host, int port) {
+    app.onRepeat(5000, [&client, host, port]() {
+      //M5.Lcd.print("?");
+      if (!client.connected()) {
+        M5.Lcd.print("*");
+        if (client.connect(host, port)) {
+          M5.Lcd.print("Reconnected to ");
+          M5.Lcd.println(host);
+          signalk_greet(client);
+        }
+      }
+    });
+  }
+
   void signalk_subscribe(WiFiClient& client) {
 
     signalk_greet(client);
 
-    app.onAvailable(client, [samples, &client]() {    
+    app.onAvailable(client, [&client]() {
       while (client.available() > 0 && client.connected()) {
         bool found = signalk_parse(client);
         if (found) {
