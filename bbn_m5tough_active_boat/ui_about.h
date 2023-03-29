3b785cda90d1463218b0d745ca3b2512fe5fe918
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-29 18:35:43 -0400

    Create ui_about.h

---
+++ bbn_m5tough_active_boat/ui_about.h
@@ -0,0 +1,23 @@
+#ifndef UI_ABOUT_H
+#define UI_ABOUT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  lv_updatable_screen_t aboutScreen;
+
+  LV_IMG_DECLARE(bareboat_necessities_logo);
+
+  void init_aboutScreen() {
+    aboutScreen.screen = lv_obj_create(NULL);  // Creates a Screen object
+    lv_obj_t *img = lv_img_create(aboutScreen.screen);
+    lv_img_set_src(img, &bareboat_necessities_logo);
+    lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
+  }
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
