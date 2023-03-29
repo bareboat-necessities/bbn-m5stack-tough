ddd9305575727e44c6de927b37eb8ef48558e7fa
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-29 13:43:04 -0400

    Update bbn_m5tough_signalk_tcp.ino

--- bbn_m5tough_signalk_tcp/bbn_m5tough_signalk_tcp.ino
+++ bbn_m5tough_signalk_tcp/bbn_m5tough_signalk_tcp.ino
@@ -66,6 +66,17 @@ void setup() {
   } else {
     M5.Lcd.println("Connection failed.");
   }
+  M5.Lcd.printf("FreeHeap: %d bytes\n", ESP.getFreeHeap());
+  M5.Lcd.printf("MinFreeHeap: %d bytes\n", ESP.getMinFreeHeap());
+  M5.Lcd.printf("FreePsram: %d bytes\n", ESP.getFreePsram());
+  M5.Lcd.printf("MinFreePsram: %d bytes\n", ESP.getMinFreePsram());
+  M5.Lcd.printf("FreeSketchSpace: %d bytes\n", ESP.getFreeSketchSpace());
+  M5.Lcd.printf("HeapSize: %d bytes\n", ESP.getHeapSize());
+
+  M5.Lcd.printf("FlashChipSize: %d bytes\n", ESP.getFlashChipSize());
+  M5.Lcd.printf("MaxAllocHeap: %d bytes\n", ESP.getMaxAllocHeap());
+  M5.Lcd.printf("MaxAllocPsram: %d bytes\n", ESP.getMaxAllocPsram());
+  M5.Lcd.printf("ChipCores: %d bytes\n", ESP.getChipCores());
 }
 
 void loop() {
