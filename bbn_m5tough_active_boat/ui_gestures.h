99dd923094e3eaaeaa0d681adcddd26698b25c5a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-29 14:45:39 -0400

    Update ui_gestures.h

--- bbn_m5tough_active_boat/ui_gestures.h
+++ bbn_m5tough_active_boat/ui_gestures.h
@@ -5,22 +5,21 @@
 extern "C" {
 #endif
 
+  Gesture swipeDown("swipe down", 80, DIR_DOWN, 40);
+  Gesture swipeUp("swipe up", 80, DIR_UP, 40);
 
-Gesture swipeDown("swipe down", 80, DIR_DOWN, 40);
-Gesture swipeUp("swipe up", 80, DIR_UP, 40);
+  Gesture swipeLeft("swipe left", 80, DIR_LEFT, 40);
+  Gesture swipeRight("swipe right", 80, DIR_RIGHT, 40);
 
-Gesture swipeLeft("swipe left", 80, DIR_LEFT, 40);
-Gesture swipeRight("swipe right", 80, DIR_RIGHT, 40);
+  bool swipe_horiz_detected() {
+    return swipeRight.wasDetected() || swipeLeft.wasDetected();
+  }
 
-bool swipe_horiz_detected() {
-  return swipeRight.wasDetected() || swipeLeft.wasDetected();
-}
+  bool swipe_vert_detected() {
+    return swipeDown.wasDetected() || swipeUp.wasDetected();
+  }
 
-bool swipe_vert_detected() {
-  return swipeDown.wasDetected() || swipeUp.wasDetected();
-}
-  
-  #ifdef __cplusplus
+#ifdef __cplusplus
 } /*extern "C"*/
 #endif
 
