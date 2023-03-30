13b655d1723ae0ddf26dd843a9032b939741f844
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-30 02:53:34 -0400

    Update bbn_m5tough_active_boat.ino

--- bbn_m5tough_active_boat/bbn_m5tough_active_boat.ino
+++ bbn_m5tough_active_boat/bbn_m5tough_active_boat.ino
@@ -1,107 +1,46 @@
-#define LV_HOR_RES_MAX 320
-#define LV_VER_RES_MAX 240
+#ifndef NET_SIGNALK_TCP_H
+#define NET_SIGNALK_TCP_H
 
-#include <M5Tough.h>
-#include <Arduino.h>
-#include <lvgl.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <WiFi.h>
-#include <Preferences.h>
-#include <ArduinoJson.h>
-#undef min(a, b)
-#include <ReactESP.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#include "ui_init.h"
-#include "ui_gestures.h"
-#include "ui_theme.h"
-#include "ui_screens.h"
-#include "m5_rtc.h"
-#include "ui_clock.h"
-#include "ui_about.h"
-
-// config store.
-Preferences preferences;
-
-#include "ui_keyboard.h"
-#include "ui_settings_wifi.h"
-#include "ui_reboot.h"
-
-using namespace reactesp;
-ReactESP app;
-
-#include "signalk_parse.h"
-#include "net_signalk_tcp.h"
-#include "ship_data_model.h"
-
-static ship_data_t shipDataModel;
-
-#include "ui_wind.h"
-#include "ui_heel.h"
-#include "ui_autopilot.h"
-#include "ui_power_victron.h"
-
-lv_updatable_screen_t* screens[] = {
-  &windScreen,
-  &aboutScreen,
-  &rebootScreen,
-  &clockScreen,
-  &victronScreen,
-  &autopilotScreen,
-  &heelScreen,
-};
-
-int page = 0;
-int pages_count = sizeof(screens) / sizeof(screens[0]);
-
-void next_page() {
-  page++;
-  if (page >= pages_count) page = 0;
-  lv_scr_load(screens[page]->screen);
-}
-
-void prev_page() {
-  page--;
-  if (page < 0) page = pages_count - 1;
-  lv_scr_load(screens[page]->screen);
-}
-
-void handle_swipe() {
-  if (swipe_vert_detected()) {
-    toggle_ui_theme();
-  } else if (swipe_right_detected()) {
-    next_page();
-  } else if (swipe_left_detected()) {
-    prev_page();
+  void signalk_greet(WiFiClient& client) {
+    String dataFeed = client.readStringUntil('\n');
+    //M5.Lcd.println(dataFeed);
+    // esp32 can't handle all volume. request only what is needed for the current screen ???
+    // const char* data = "{\"context\": \"*\",\"subscribe\": [{\"path\": \"navigation.rateOfTurn\"}]}";
+    const char* data = "{\"context\": \"*\",\"subscribe\": [{\"path\": \"*\"}]}";
+    client.println(data);
+    client.flush();
   }
-}
 
-void setup() {
-  tft_lv_initialization();
-  init_disp_driver();
-  init_touch_driver();
-  init_theme();
-
-  settingUpWiFi([&page, &screens](){
-    init_windScreen();
-    init_aboutScreen();
-    init_rebootScreen();
-    init_clockScreen();
-    init_victronScreen();
-    init_autopilotScreen();
-    init_heelScreen();
-    lv_scr_load(screens[page]->screen);
-  });
-}
-
-void loop() {
-  M5.update();
-  lv_task_handler();
-  app.tick();
-  lv_tick_inc(1);
-
-  if (!settingMode) {
-    handle_swipe();
-    update_screen(*screens[page]);
+  void setup_reconnect(WiFiClient& client, const char* host, int port) {
+    app.onRepeat(5000, [&client, host, port]() {
+      if (!client.connected()) {
+        if (client.connect(host, port)) {
+          signalk_greet(client);
+        }
+      }
+    });
   }
-}
+
+  void signalk_subscribe(WiFiClient& client) {
+
+    signalk_greet(client);
+
+    app.onAvailable(client, [&client]() {
+      while (client.available() > 256 /* Very important for performance and responsiveness */ && client.connected()) {
+        bool found = signalk_parse(client);
+        if (found) {
+          break;
+        }
+      }
+    });
+  }
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
