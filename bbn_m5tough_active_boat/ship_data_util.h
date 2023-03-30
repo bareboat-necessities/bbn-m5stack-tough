f4c796052ae61869c3a0839d79d45867e9a0d88b
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-30 03:31:09 -0400

    Update ship_data_util.h

--- bbn_m5tough_active_boat/ship_data_util.h
+++ bbn_m5tough_active_boat/ship_data_util.h
@@ -1,1 +1,16 @@
+#ifndef SHIP_DATA_UTIL_H
+#define SHIP_DATA_UTIL_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  bool fresh(unsigned long age) {
+    return millis() - age < 5000;
+  }
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
