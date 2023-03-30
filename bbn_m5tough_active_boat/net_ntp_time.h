d6ae5dfd5473fdd702e97ac864b7227531a0ab3f
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-30 11:32:28 -0400

    Update net_ntp_time.h

--- bbn_m5tough_active_boat/net_ntp_time.h
+++ bbn_m5tough_active_boat/net_ntp_time.h
@@ -1,1 +1,36 @@
+#ifndef NET_NTP_TIME_H
+#define NET_NTP_TIME_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  const char* ntpServer = "pool.ntp.org";
+  const long utcOffset_sec = 0;
+  const int daylightOffset_sec = 0;  //3600;
+
+  void setupTime() {
+    struct tm timeinfo;
+    if (getLocalTime(&timeinfo)) {
+      RTCdate.Year = timeinfo.tm_year;
+      RTCdate.Month = timeinfo.tm_mon;
+      RTCdate.Date = timeinfo.tm_mday;
+      M5.Rtc.SetDate(&RTCdate);
+
+      RTCtime.Hours = timeinfo.tm_hour;
+      RTCtime.Minutes = timeinfo.tm_min;
+      RTCtime.Seconds = timeinfo.tm_sec;
+      M5.Rtc.SetTime(&RTCtime);  // writes the  time to the (RTC) real time clock.
+    }
+  }
+
+  void init_dateTime() {
+    configTime(utcOffset_sec, daylightOffset_sec, ntpServer);
+    setupTime(); 
+  }
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
