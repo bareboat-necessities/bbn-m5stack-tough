df703d9091261600636614b836f57cfb21639c81
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-30 19:53:45 -0400

    Update pypilot_parse.h

--- bbn_m5tough_active_boat/pypilot_parse.h
+++ bbn_m5tough_active_boat/pypilot_parse.h
@@ -1,1 +1,17 @@
+#ifndef PYPILOT_PARSE_H
+#define PYPILOT_PARSE_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  bool pypilot_parse(Stream& stream) {
+    bool found = false;
+    return found;
+  }
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
