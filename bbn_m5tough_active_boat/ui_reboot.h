9e50cd78b4e519fbbfb1e0c73cf3dc7492348d83
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-30 00:24:58 -0400

    Update ui_reboot.h

--- bbn_m5tough_active_boat/ui_reboot.h
+++ bbn_m5tough_active_boat/ui_reboot.h
@@ -25,15 +25,15 @@ extern "C" {
 
     lv_obj_t *btn1 = lv_btn_create(rebootScreen.screen);
     lv_obj_t *label1 = lv_label_create(btn1);
-    lv_obj_align(btn1, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_align(btn1, LV_ALIGN_CENTER, 0, 10);
     lv_label_set_text(label1, "Reboot Device");
     lv_obj_center(label1);
     lv_obj_add_event_cb(btn1, btnReboot_event, LV_EVENT_PRESSED, NULL);
 
     lv_obj_t *btn2 = lv_btn_create(rebootScreen.screen);
     lv_obj_t *label2 = lv_label_create(btn2);
-    lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 50);
-    lv_label_set_text(label2, "Power Off");
+    lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 70);
+    lv_label_set_text(label2, LV_SYMBOL_POWER);
     lv_obj_center(label2);
     lv_obj_add_event_cb(btn2, btnPowerOff_event, LV_EVENT_LONG_PRESSED, NULL);
 
