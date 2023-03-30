e5d30d366b83f9b62922c3e8c73c9d4cf6fa2aee
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-30 14:27:13 -0400

    Update ui_heel.h

--- bbn_m5tough_active_boat/ui_heel.h
+++ bbn_m5tough_active_boat/ui_heel.h
@@ -8,6 +8,8 @@ extern "C" {
   lv_updatable_screen_t heelScreen;
 
   static lv_obj_t *heel_display;
+  static lv_meter_indicator_t *indic_heel;
+  static lv_obj_t *pitch_label;
 
   static void set_heel_value(void *indic, int32_t v) {
     lv_meter_set_indicator_value(heel_display, (lv_meter_indicator_t *)indic, v);
@@ -31,27 +33,24 @@ extern "C" {
     lv_meter_set_scale_ticks(heel_display, scale, 19, 2, 10, lv_palette_main(LV_PALETTE_GREY));
     lv_meter_set_scale_major_ticks(heel_display, scale, 3, 3, 20, lv_palette_main(LV_PALETTE_GREY), 15);
 
-    lv_meter_indicator_t *indic;
-
     /*Add a needle line indicator*/
-    indic = lv_meter_add_needle_line(heel_display, scale, 7, lv_palette_main(LV_PALETTE_ORANGE), -10);
+    indic_heel = lv_meter_add_needle_line(heel_display, scale, 7, lv_palette_main(LV_PALETTE_ORANGE), -10);
 
-    /*Create an animation to set the value*/
-    lv_anim_t a;
-    lv_anim_init(&a);
-    lv_anim_set_exec_cb(&a, set_heel_value);
-    lv_anim_set_var(&a, indic);
-    lv_anim_set_values(&a, 5, 15);
-    lv_anim_set_time(&a, 5000);
-    lv_anim_set_repeat_delay(&a, 3000);
-    lv_anim_set_playback_time(&a, 5000);
-    lv_anim_set_playback_delay(&a, 3000);
-    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
-    lv_anim_start(&a);
+    pitch_label = lv_label_create(parent);
+    lv_obj_align(pitch_label, LV_ALIGN_TOP_LEFT, 5, 5);
+
+    lv_obj_t *main_label = lv_label_create(parent);
+    lv_obj_align(main_label, LV_ALIGN_CENTER, 0, -60);
+    lv_label_set_text(main_label, "HEEL");
   }
 
   static void heel_update_cb() {
-    // TODO:
+    lv_label_set_text(pitch_label,
+                      (String("PITCH: ")
+                       + (fresh(shipDataModel.navigation.attitude.pitch.age) ? String(shipDataModel.navigation.attitude.pitch.deg) + String(LV_SYMBOL_DEGREES) : String("n/a")))
+                        .c_str());
+
+    set_heel_value(indic_heel, fresh(shipDataModel.navigation.attitude.heel.age) ? shipDataModel.navigation.attitude.heel.deg : 0);
   }
 
   void init_heelScreen() {
