4f08a9d10970fd88dd4bd500b052570a8f73b5b9
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-31 10:53:21 -0400

    Update net_nmea0183_tcp.h

--- bbn_m5tough_active_boat/net_nmea0183_tcp.h
+++ bbn_m5tough_active_boat/net_nmea0183_tcp.h
@@ -1,1 +1,33 @@
+#ifndef NET_NMEA0183_TCP_H
+#define NET_NMEA0183_TCP_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  void setup_nmea0183_reconnect(WiFiClient& client, const char* host, int port) {
+    app.onRepeat(5000, [&client, host, port]() {
+      if (!client.connected()) {
+        if (client.connect(host, port)) {
+        }
+      }
+    });
+  }
+
+  void nmea0183_subscribe(WiFiClient& client) {
+
+    app.onAvailable(client, [&client]() {
+      while (client.available() > 64 /* Very important for performance and responsiveness */ && client.connected()) {
+        bool found = nmea0183_parse(client);
+        if (found) {
+          break; /* Very important for performance and responsiveness */
+        }
+      }
+    });
+  }
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
