7a8eee48f70eeadd7bf2fa0d4bdfc4a8b1be806a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-30 20:56:10 -0400

    Update pypilot_parse.h

--- bbn_m5tough_active_boat/pypilot_parse.h
+++ bbn_m5tough_active_boat/pypilot_parse.h
@@ -14,13 +14,17 @@ extern "C" {
     // ap.mode="compass"
     if (dataFeed.length() > 0) {
       if (dataFeed.startsWith("ap.heading=")) {
-
+        // TODO:
       } else if (dataFeed.startsWith("ap.heading_command=")) {
-
-      } else if (dataFeed.startsWith("ap.enabled=")) {
-
+        // TODO:
+      } else if (dataFeed.startsWith("ap.enabled=true")) {
+        shipDataModel.steering.autopilot.ap_state.st = ap_state_e::ENGAGED;
+        shipDataModel.steering.autopilot.ap_state.age = millis();
+      } else if (dataFeed.startsWith("ap.enabled=false")) {
+        shipDataModel.steering.autopilot.ap_state.st = ap_state_e::STANDBY;
+        shipDataModel.steering.autopilot.ap_state.age = millis();
       } else if (dataFeed.startsWith("ap.mode=\"")) {
-
+        // TODO:
       }
       return found;
     }
