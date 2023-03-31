b0ad0f31e641ba672a92cda68aaeb4c15ccb62fa
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-30 23:19:50 -0400

    Update ui_brightness.h

--- bbn_m5tough_active_boat/ui_brightness.h
+++ bbn_m5tough_active_boat/ui_brightness.h
@@ -1,1 +1,25 @@
+#ifndef UI_BRIGHTNESS_H
+#define UI_BRIGHTNESS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  int lcdVoltage = 2900;
+
+  static void btnBacklight_event(lv_event_t *event) {
+    // TODO:
+    if (lcdVoltage > 2900) {
+      lcdVoltage = 2500;
+    } else {
+      lcdVoltage = 3100;
+    }
+    M5.Axp.SetLDOVoltage(3, lcdVoltage);
+  }
+
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
