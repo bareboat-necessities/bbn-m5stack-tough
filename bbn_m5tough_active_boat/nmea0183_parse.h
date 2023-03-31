62c41fc4f3a6f6f24a9afd8843704700f44b59ed
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-31 10:52:45 -0400

    Update nmea0183_parse.h

--- bbn_m5tough_active_boat/nmea0183_parse.h
+++ bbn_m5tough_active_boat/nmea0183_parse.h
@@ -1,1 +1,18 @@
+#ifndef NMEA0183_PARSE_H
+#define NMEA0183_PARSE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  bool nmea0183_parse(WiFiClient& client) {
+    bool found = false;
+    String dataFeed = client.readStringUntil('\n');
+  }
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
 
