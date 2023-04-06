4b7f5f4164a59c846311a0fbc3d40216a3641cb7
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-06 13:43:42 -0400

    Update ui_speed.h

--- bbn_m5tough_active_boat/ui_speed.h
+++ bbn_m5tough_active_boat/ui_speed.h
@@ -8,6 +8,9 @@ extern "C" {
   lv_updatable_screen_t speedScreen;
 
   static lv_obj_t *sog_label;
+  static lv_obj_t *sog_avg_label;
+  static lv_obj_t *spd_label;
+  static lv_obj_t *g_force_label;
 
   /**
    * A speed display 
@@ -15,8 +18,36 @@ extern "C" {
   void lv_speed_display(lv_obj_t *parent) {
 
     lv_obj_t *main_label = lv_label_create(parent);
-    lv_obj_align(main_label, LV_ALIGN_CENTER, 0, -60);
+    lv_obj_align(main_label, LV_ALIGN_CENTER, 0, -110);
     lv_label_set_text(main_label, "SPEED");
+
+    sog_label = lv_label_create(parent);
+    lv_obj_align(sog_label, LV_ALIGN_TOP_LEFT, 10, 40);
+#if LV_FONT_MONTSERRAT_20
+    lv_obj_set_style_text_font(sog_label, &lv_font_montserrat_20, NULL);
+#endif
+    lv_label_set_text(sog_label, "SOG (kn):                       --");
+
+    sog_avg_label = lv_label_create(parent);
+    lv_obj_align(sog_avg_label, LV_ALIGN_TOP_LEFT, 10, 80);
+#if LV_FONT_MONTSERRAT_20
+    lv_obj_set_style_text_font(sog_avg_label, &lv_font_montserrat_20, NULL);
+#endif
+    lv_label_set_text(sog_avg_label, "SOG AVG (kn):             --");
+
+    spd_label = lv_label_create(parent);
+    lv_obj_align(spd_label, LV_ALIGN_TOP_LEFT, 10, 120);
+#if LV_FONT_MONTSERRAT_20
+    lv_obj_set_style_text_font(spd_label, &lv_font_montserrat_20, NULL);
+#endif
+    lv_label_set_text(spd_label, "SPD (kn):                       --");
+
+    g_force_label = lv_label_create(parent);
+    lv_obj_align(g_force_label, LV_ALIGN_TOP_LEFT, 10, 160);
+#if LV_FONT_MONTSERRAT_20
+    lv_obj_set_style_text_font(g_force_label, &lv_font_montserrat_20, NULL);
+#endif
+    lv_label_set_text(g_force_label, "G Force (% of g):         --");
   }
 
   static void speed_update_cb() {
