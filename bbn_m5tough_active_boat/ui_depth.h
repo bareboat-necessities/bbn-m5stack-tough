f485c80af5ee1f43340a8c922d3fe7ce1f2b0957
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-06 12:07:54 -0400

    Update ui_depth.h

--- bbn_m5tough_active_boat/ui_depth.h
+++ bbn_m5tough_active_boat/ui_depth.h
@@ -9,6 +9,7 @@ extern "C" {
 
   static lv_obj_t *dbt_label;
   static lv_obj_t *dbk_label;
+  static lv_obj_t *dbs_label;
 
   /**
    * A depth display 
@@ -16,8 +17,29 @@ extern "C" {
   void lv_depth_display(lv_obj_t *parent) {
 
     lv_obj_t *main_label = lv_label_create(parent);
-    lv_obj_align(main_label, LV_ALIGN_CENTER, 0, -60);
+    lv_obj_align(main_label, LV_ALIGN_CENTER, 0, -110);
     lv_label_set_text(main_label, "DEPTH");
+
+    dbt_label = lv_label_create(parent);
+    lv_obj_align(dbt_label, LV_ALIGN_TOP_LEFT, 10, 40);
+#if LV_FONT_MONTSERRAT_20
+    lv_obj_set_style_text_font(dbt_label, &lv_font_montserrat_20, NULL);
+#endif
+    lv_label_set_text(dbt_label, "DBT (ft):     --");
+
+    dbk_label = lv_label_create(parent);
+    lv_obj_align(dbk_label, LV_ALIGN_TOP_LEFT, 10, 80);
+#if LV_FONT_MONTSERRAT_20
+    lv_obj_set_style_text_font(dbk_label, &lv_font_montserrat_20, NULL);
+#endif
+    lv_label_set_text(dbk_label, "DBK (ft):     --");
+
+    dbs_label = lv_label_create(parent);
+    lv_obj_align(dbs_label, LV_ALIGN_TOP_LEFT, 10, 120);
+#if LV_FONT_MONTSERRAT_20
+    lv_obj_set_style_text_font(dbs_label, &lv_font_montserrat_20, NULL);
+#endif
+    lv_label_set_text(dbs_label, "DBS (ft):     --");
   }
 
   static void depth_update_cb() {
