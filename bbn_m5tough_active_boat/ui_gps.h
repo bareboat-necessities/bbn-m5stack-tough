95284a43fa1518b137d0f35861ac6783356132f9
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-06 14:57:39 -0400

    Update ui_gps.h

--- bbn_m5tough_active_boat/ui_gps.h
+++ bbn_m5tough_active_boat/ui_gps.h
@@ -7,9 +7,10 @@ extern "C" {
 
   lv_updatable_screen_t gpsScreen;
 
-  static lv_obj_t *cog_label;
-  static lv_obj_t *lat_label;
-  static lv_obj_t *lon_label;
+  static lv_obj_t *gps_time_label;
+  static lv_obj_t *gps_cog_label;
+  static lv_obj_t *gps_lat_label;
+  static lv_obj_t *gps_lon_label;
 
   /**
    * A GPS display 
@@ -17,8 +18,29 @@ extern "C" {
   void lv_gps_display(lv_obj_t *parent) {
 
     lv_obj_t *main_label = lv_label_create(parent);
-    lv_obj_align(main_label, LV_ALIGN_CENTER, 0, -60);
+    lv_obj_align(main_label, LV_ALIGN_CENTER, 0, -110);
     lv_label_set_text(main_label, "GPS");
+
+    gps_time_label = lv_label_create(parent);
+    lv_obj_align(gps_time_label, LV_ALIGN_TOP_LEFT, 10, 40);
+#if LV_FONT_MONTSERRAT_20
+    lv_obj_set_style_text_font(gps_time_label, &lv_font_montserrat_20, NULL);
+#endif
+    lv_label_set_text(gps_time_label, "GPS TIME:        --");
+
+    gps_lat_label = lv_label_create(parent);
+    lv_obj_align(gps_lat_label, LV_ALIGN_TOP_LEFT, 10, 80);
+#if LV_FONT_MONTSERRAT_20
+    lv_obj_set_style_text_font(gps_lat_label, &lv_font_montserrat_20, NULL);
+#endif
+    lv_label_set_text(gps_lat_label, "LAT:                    --");
+
+    gps_lon_label = lv_label_create(parent);
+    lv_obj_align(gps_lon_label, LV_ALIGN_TOP_LEFT, 10, 120);
+#if LV_FONT_MONTSERRAT_20
+    lv_obj_set_style_text_font(gps_lon_label, &lv_font_montserrat_20, NULL);
+#endif
+    lv_label_set_text(gps_lon_label, "LON:                   --");
   }
 
   static void gps_update_cb() {
@@ -35,4 +57,3 @@ extern "C" {
 #endif
 
 #endif
-
