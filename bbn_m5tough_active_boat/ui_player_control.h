ba11b811c006e5c6fd467fe9253111c20007e07a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-07 11:10:38 -0400

    Update ui_player_control.h

--- bbn_m5tough_active_boat/ui_player_control.h
+++ bbn_m5tough_active_boat/ui_player_control.h
@@ -1,1 +1,15 @@
+#ifndef UI_PLAYER_CONTROL_H
+#define UI_PLAYER_CONTROL_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  lv_updatable_screen_t playerScreen;
+
+  
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
