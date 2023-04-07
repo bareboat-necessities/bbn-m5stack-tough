c5f5857388fd004efeb5d68a47b2ef3ca815fd55
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-07 00:50:25 -0400

    Update net_mpd_tcp.h

--- bbn_m5tough_active_boat/net_mpd_tcp.h
+++ bbn_m5tough_active_boat/net_mpd_tcp.h
@@ -1,1 +1,48 @@
+/*
 
+
+telnet lysmarine 6600
+Trying 192.168.1.34...
+Connected to lysmarine.fios-router.home.
+Escape character is '^]'.
+OK MPD 0.19.0
+status
+volume: 100
+repeat: 0
+random: 0
+single: 0
+consume: 0
+playlist: 0
+playlistlength: 0
+xfade: 0
+state: stop
+OK
+idle
+changed: playlist
+OK
+idle
+changed: mixer
+changed: playlist
+changed: player
+OK
+status
+volume: 100
+repeat: 0
+random: 0
+single: 0
+consume: 0
+playlist: 2
+playlistlength: 6
+xfade: 0
+state: play
+song: 0
+songid: 1
+nextsong: 1
+nextsongid: 2
+time: 5:205
+elapsed: 5.986
+bitrate: 0
+OK
+idle
+
+*/
