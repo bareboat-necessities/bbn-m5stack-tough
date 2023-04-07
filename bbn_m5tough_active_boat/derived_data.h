1ae4c4c3cf6196e2fb20eb395d94c04f53714074
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-06 22:54:11 -0400

    Update derived_data.h

--- bbn_m5tough_active_boat/derived_data.h
+++ bbn_m5tough_active_boat/derived_data.h
@@ -1,1 +1,15 @@
+#ifndef DERIVED_DATA_H
+#define DERIVED_DATA_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  static void derive_data() {
+  }
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
