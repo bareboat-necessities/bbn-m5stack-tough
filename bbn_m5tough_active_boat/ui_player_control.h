327876bdbdac9f24103059759282052d966f38fb
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-07 11:17:51 -0400

    Update ui_player_control.h

--- bbn_m5tough_active_boat/ui_player_control.h
+++ bbn_m5tough_active_boat/ui_player_control.h
@@ -9,30 +9,35 @@ extern "C" {
 
   static lv_obj_t *player_display;
 
-static const char *btnm_map[] = {
-  LV_SYMBOL_SHUFFLE,
-  LV_SYMBOL_PREV,
-  LV_SYMBOL_PLAY,
-  LV_SYMBOL_NEXT,
-  LV_SYMBOL_LOOP,
-  "",
-};
+  static const char *player_btnm_map[] = {
+    LV_SYMBOL_SHUFFLE,
+    LV_SYMBOL_PREV,
+    LV_SYMBOL_PLAY,
+    LV_SYMBOL_NEXT,
+    LV_SYMBOL_LOOP,
+    "",
+  };
 
-/**
+  /**
  * A player display 
  */
-void lv_player_display(lv_obj_t *parent) {
-  lv_obj_t *player_display = lv_btnmatrix_create(parent);
-  lv_btnmatrix_set_map(player_display, btnm_map);
-   lv_obj_set_size(player_display, 320, 78);
-  //lv_btnmatrix_set_btn_width(player_display, 15, 2);
-  //lv_btnmatrix_set_btn_height(player_display, 15, 2);
-  lv_obj_align(player_display, LV_ALIGN_CENTER, 0, 75);
-  //lv_obj_add_event(player_display, event_handler, LV_EVENT_ALL, NULL);
-}
+  void lv_player_display(lv_obj_t *parent) {
+    lv_obj_t *player_display = lv_btnmatrix_create(parent);
+    lv_btnmatrix_set_map(player_display, player_btnm_map);
+    lv_obj_set_size(player_display, 320, 78);
+    lv_obj_align(player_display, LV_ALIGN_CENTER, 0, 75);
+    //lv_obj_add_event(player_display, event_handler, LV_EVENT_ALL, NULL);
+  }
 
+  static void player_update_cb() {
+  }
 
-  
+  void init_playerScreen() {
+    playerScreen.screen = lv_obj_create(NULL);  // Creates a Screen object
+    lv_player_display(playerScreen.screen);
+    playerScreen.update_cb = player_update_cb;
+  }
+
 #ifdef __cplusplus
 } /*extern "C"*/
 #endif
