f284985fe943534414f9d84562e9e88c0b8eb98f
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-06 23:33:29 -0400

    Update derived_data.h

--- bbn_m5tough_active_boat/derived_data.h
+++ bbn_m5tough_active_boat/derived_data.h
@@ -6,6 +6,16 @@ extern "C" {
 #endif
 
   static void derive_data() {
+    // derive magnetic variation
+    if (fresh(shipDataModel.navigation.position.lat.age)
+        && fresh(shipDataModel.navigation.position.lon.age)) {
+      M5.Rtc.GetDate(&RTCdate);
+      float longitude = shipDataModel.navigation.position.lon.deg;
+      float latitude = shipDataModel.navigation.position.lat.deg;
+      float mag_var_deg = myDeclination.magneticDeclination(latitude, longitude, RTCdate.Year % 100, 1 + RTCdate.Month, RTCdate.Date);
+      shipDataModel.navigation.mag_var.deg = mag_var_deg;
+      shipDataModel.navigation.mag_var.age = millis();
+    }
   }
 
 #ifdef __cplusplus
