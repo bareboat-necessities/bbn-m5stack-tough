e6bd465d8ba4264fe8455f6fc29ea76ea6013ef9
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-07 02:23:14 -0400

    Update leeway.h

--- bbn_m5tough_active_boat/leeway.h
+++ bbn_m5tough_active_boat/leeway.h
@@ -13,13 +13,13 @@ extern "C" {
     return abs(hdt - cogt);
   }
  
-  // Empirical formula for leeway
+  // Empirical formula for leeway.
   // Leeway (deg) is angle to adjust heading to maintain constant COG (assuming no current)
-  // kFactor - Leeway correlation constant, typically from 9 to 16 (9 for super racer)
+  // k_factor - leeway correlation constant (is boat and boat load specific constant), typically from 9 to 16 (9 for super racer)
   // heel_deg - heel angle in degrees
   // stw_kn - speed through water (aka LOG or SPD) in knots
-  static float leeway(float heel_deg, float stw_kn, int kFactor = 12) {
-    float leeway_deg = kFactor * heel_deg / (stw_kn * stw_kn);
+  static float leeway(float heel_deg, float stw_kn, int k_factor = 12) {
+    float leeway_deg = k_factor * heel_deg / (stw_kn * stw_kn);
     retun leeway_deg;
   }
 
