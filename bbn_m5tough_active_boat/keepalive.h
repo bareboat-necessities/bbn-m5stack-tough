0c6d79588b6528c0c9715cc3796ae8e99d85bb9e
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-08 18:34:46 -0400

    Update keepalive.h

--- bbn_m5tough_active_boat/keepalive.h
+++ bbn_m5tough_active_boat/keepalive.h
@@ -9,13 +9,13 @@ extern "C" {
 
   void setKeepAlive(WiFiClient &wclient) {
     int flags = 1;
-    wclient.setSocketOption(SOL_SOCKET, SO_KEEPALIVE, (char *)&flags, sizeof(flags));
+    wclient.setSocketOption(SOL_SOCKET, SO_KEEPALIVE, (const void *)&flags, sizeof(flags));
     flags = 10;
-    wclient.setSocketOption(IPPROTO_TCP, TCP_KEEPIDLE, (char *)&flags, sizeof(flags));
+    wclient.setSocketOption(IPPROTO_TCP, TCP_KEEPIDLE, (const void *)&flags, sizeof(flags));
     flags = 5;
-    wclient.setSocketOption(IPPROTO_TCP, TCP_KEEPCNT, (void *)&flags, sizeof(flags));
+    wclient.setSocketOption(IPPROTO_TCP, TCP_KEEPCNT, (const void *)&flags, sizeof(flags));
     flags = 5;
-    wclient.setSocketOption(IPPROTO_TCP, TCP_KEEPINTVL, (void *)&flags, sizeof(flags));
+    wclient.setSocketOption(IPPROTO_TCP, TCP_KEEPINTVL, (const void *)&flags, sizeof(flags));
   }
 
 #ifdef __cplusplus
