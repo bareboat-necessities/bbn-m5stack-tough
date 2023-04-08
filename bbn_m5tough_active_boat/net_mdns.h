d222623ac6d0a80baf163347be159a8a102ab8e7
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-08 14:55:02 -0400

    Update net_mdns.h

--- bbn_m5tough_active_boat/net_mdns.h
+++ bbn_m5tough_active_boat/net_mdns.h
@@ -8,17 +8,17 @@
 extern "C" {
 #endif
 
-#define SK_TCP_HOST_PREF "signalk_tcp.host"
-#define SK_TCP_PORT_PREF "signalk_tcp.port"
+#define SK_TCP_HOST_PREF "signalk_host"
+#define SK_TCP_PORT_PREF "signalk_port"
 
-#define PYP_TCP_HOST_PREF "pypilot_tcp.host"
-#define PYP_TCP_PORT_PREF "pypilot_tcp.port"
+#define PYP_TCP_HOST_PREF "pypilot_host"
+#define PYP_TCP_PORT_PREF "pypilot_port"
 
-#define NMEA0183_TCP_HOST_PREF "nmea0183_tcp.host"
-#define NMEA0183_TCP_PORT_PREF "nmea0183_tcp.port"
+#define NMEA0183_TCP_HOST_PREF "n0183_host"
+#define NMEA0183_TCP_PORT_PREF "n0183_port"
 
-#define MPD_TCP_HOST_PREF "mpd_tcp.host"
-#define MPD_TCP_PORT_PREF "mpd_tcp.port"
+#define MPD_TCP_HOST_PREF "mpd_host"
+#define MPD_TCP_PORT_PREF "mpd_port"
 
   void mdns_begin() {
     if (!MDNS.begin("ESP32_Browser")) {
@@ -31,7 +31,7 @@ extern "C" {
   }
 
   void discover_n_config() {
-    preferences.begin("clients_config", false);
+    bool saved = false;
 
     String signalk_tcp_host = preferences.getString(SK_TCP_HOST_PREF);
     int signalk_tcp_port = preferences.getInt(SK_TCP_PORT_PREF);
@@ -39,8 +39,11 @@ extern "C" {
       int n = MDNS.queryService("signalk-tcp", "tcp");
       if (n > 0) {
         if (n == 1) {
+          preferences.remove("SK_TCP_HOST_PREF");
+          preferences.remove("SK_TCP_PORT_PREF");
           preferences.putString(SK_TCP_HOST_PREF, MDNS.IP(0).toString());
           preferences.putInt(SK_TCP_PORT_PREF, MDNS.port(0));
+          saved = true;
           M5.Lcd.print(MDNS.IP(0).toString());
           M5.Lcd.print(":");
           M5.Lcd.printf("%d - signalk\n", MDNS.port(0));
@@ -54,8 +57,11 @@ extern "C" {
       int n = MDNS.queryService("nmea-0183", "tcp");
       if (n > 0) {
         if (n == 1) {
+          preferences.remove("NMEA0183_TCP_HOST_PREF");
+          preferences.remove("NMEA0183_TCP_PORT_PREF");
           preferences.putString(NMEA0183_TCP_HOST_PREF, MDNS.IP(0).toString());
           preferences.putInt(NMEA0183_TCP_PORT_PREF, MDNS.port(0));
+          saved = true;
           M5.Lcd.print(MDNS.IP(0).toString());
           M5.Lcd.print(":");
           M5.Lcd.printf("%d - nmea0183\n", MDNS.port(0));
@@ -68,14 +74,20 @@ extern "C" {
             break;
           }
           if (found10110 != NULL && found10110.length() > 0) {
+            preferences.remove("NMEA0183_TCP_HOST_PREF");
+            preferences.remove("NMEA0183_TCP_PORT_PREF");
             preferences.putString(NMEA0183_TCP_HOST_PREF, found10110);
             preferences.putInt(NMEA0183_TCP_PORT_PREF, 10110);
+            saved = true;
             M5.Lcd.print(found10110);
             M5.Lcd.print(":");
             M5.Lcd.printf("%d - nmea0183\n", 10110);
           } else {
+            preferences.remove("NMEA0183_TCP_HOST_PREF");
+            preferences.remove("NMEA0183_TCP_PORT_PREF");
             preferences.putString(NMEA0183_TCP_HOST_PREF, MDNS.IP(0).toString());
             preferences.putInt(NMEA0183_TCP_PORT_PREF, MDNS.port(0));
+            saved = true;
             M5.Lcd.print(MDNS.IP(0).toString());
             M5.Lcd.print(":");
             M5.Lcd.printf("%d - nmea0183\n", MDNS.port(0));
@@ -90,8 +102,11 @@ extern "C" {
       int n = MDNS.queryService("pypilot", "tcp");
       if (n > 0) {
         if (n == 1) {
+          preferences.remove("PYP_TCP_HOST_PREF");
+          preferences.remove("PYP_TCP_HOST_PREF");
           preferences.putString(PYP_TCP_HOST_PREF, MDNS.IP(0).toString());
           preferences.putInt(PYP_TCP_PORT_PREF, MDNS.port(0));
+          saved = true;
           M5.Lcd.print(MDNS.IP(0).toString());
           M5.Lcd.print(":");
           M5.Lcd.printf("%d - pypilot\n", MDNS.port(0));
@@ -105,15 +120,20 @@ extern "C" {
       int n = MDNS.queryService("mpd", "tcp");
       if (n > 0) {
         if (n == 1) {
+          preferences.remove("MPD_TCP_HOST_PREF");
+          preferences.remove("MPD_TCP_PORT_PREF");
           preferences.putString(MPD_TCP_HOST_PREF, MDNS.IP(0).toString());
           preferences.putInt(MPD_TCP_PORT_PREF, MDNS.port(0));
+          saved = true;
           M5.Lcd.print(MDNS.IP(0).toString());
           M5.Lcd.print(":");
           M5.Lcd.printf("%d - mpd\n", MDNS.port(0));
         }
       }
     }
-    preferences.end();
+//    if (saved) {
+//      ESP.restart();
+//    }
   }
 
   void browseServiceMDNS(const char* service, const char* proto) {
