1ef1f42ba57a67202848e798cdc33f185d72797b
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-08 22:44:44 -0400

    Update ui_mem_cpu_net_stat.h

--- bbn_m5tough_active_boat/ui_mem_cpu_net_stat.h
+++ bbn_m5tough_active_boat/ui_mem_cpu_net_stat.h
@@ -31,18 +31,42 @@ extern "C" {
   void lv_dev_status_display(lv_obj_t *parent) {
     pyp_status_label = lv_label_create(parent);
     lv_obj_align(pyp_status_label, LV_ALIGN_TOP_LEFT, 20, 30);
-    lv_label_set_text(pyp_status_label, (String("PyPilot:              ") + preferences.getString(PYP_TCP_HOST_PREF) + String(":") + String(preferences.getInt(PYP_TCP_PORT_PREF))).c_str());
+    lv_label_set_recolor(pyp_status_label, true);
+    lv_label_set_text(pyp_status_label,
+                      (String("#ff0000 " LV_SYMBOL_OK "  #") + String("PyPilot:              ") + preferences.getString(PYP_TCP_HOST_PREF) + String(":") + String(preferences.getInt(PYP_TCP_PORT_PREF))).c_str());
 
     signalk_status_label = lv_label_create(parent);
     lv_obj_align(signalk_status_label, LV_ALIGN_TOP_LEFT, 20, 60);
-    lv_label_set_text(signalk_status_label, (String("SignalK:             ") + preferences.getString(SK_TCP_HOST_PREF) + String(":") + String(preferences.getInt(SK_TCP_PORT_PREF))).c_str());
+    lv_label_set_recolor(signalk_status_label, true);
+    lv_label_set_text(signalk_status_label,
+                      (String("#ff0000 " LV_SYMBOL_OK "  #") + String("SignalK:             ") + preferences.getString(SK_TCP_HOST_PREF) + String(":") + String(preferences.getInt(SK_TCP_PORT_PREF))).c_str());
 
     nmea0183_status_label = lv_label_create(parent);
     lv_obj_align(nmea0183_status_label, LV_ALIGN_TOP_LEFT, 20, 90);
-    lv_label_set_text(nmea0183_status_label, (String("NMEA 0183:       ") + preferences.getString(NMEA0183_TCP_HOST_PREF) + String(":") + String(preferences.getInt(NMEA0183_TCP_PORT_PREF))).c_str());
+    lv_label_set_recolor(nmea0183_status_label, true);
+    lv_label_set_text(nmea0183_status_label,
+                      (String("#ff0000 " LV_SYMBOL_OK "  #") + String("NMEA 0183:       ") + preferences.getString(NMEA0183_TCP_HOST_PREF) + String(":") + String(preferences.getInt(NMEA0183_TCP_PORT_PREF))).c_str());
   }
 
   static void dev_status_update_cb() {
+    lv_label_set_text(pyp_status_label,
+                      ((pypClient.connected() ? String("#00ff00 " LV_SYMBOL_OK "  #")
+                                              : String("#ff0000 " LV_SYMBOL_OK "  #"))
+                       + String("PyPilot:              ") + preferences.getString(PYP_TCP_HOST_PREF)
+                       + String(":") + String(preferences.getInt(PYP_TCP_PORT_PREF)))
+                        .c_str());
+    lv_label_set_text(signalk_status_label,
+                      ((skClient.connected() ? String("#00ff00 " LV_SYMBOL_OK "  #")
+                                             : String("#ff0000 " LV_SYMBOL_OK "  #"))
+                       + String("SignalK:             ") + preferences.getString(SK_TCP_HOST_PREF)
+                       + String(":") + String(preferences.getInt(SK_TCP_PORT_PREF)))
+                        .c_str());
+    lv_label_set_text(nmea0183_status_label,
+                      ((nmea0183Client.connected() ? String("#00ff00 " LV_SYMBOL_OK "  #")
+                                                   : String("#ff0000 " LV_SYMBOL_OK "  #"))
+                       + String("NMEA 0183:       ") + preferences.getString(NMEA0183_TCP_HOST_PREF)
+                       + String(":") + String(preferences.getInt(NMEA0183_TCP_PORT_PREF)))
+                        .c_str());
   }
 
   void init_devStatusScreen() {
