64b430142a83439ffe777f04b048258cfc02b7f8
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-08 22:21:17 -0400

    Update ui_mem_cpu_net_stat.h

--- bbn_m5tough_active_boat/ui_mem_cpu_net_stat.h
+++ bbn_m5tough_active_boat/ui_mem_cpu_net_stat.h
@@ -5,6 +5,12 @@
 extern "C" {
 #endif
 
+  lv_updatable_screen_t devStatusScreen;
+
+  static lv_obj_t *pyp_status_label;
+  static lv_obj_t *nmea0183_status_label;
+  static lv_obj_t *signalk_status_label;
+
   void printDeviceStats() {
     M5.Lcd.printf("FreeHeap: %d bytes\n", ESP.getFreeHeap());
     M5.Lcd.printf("MinFreeHeap: %d bytes\n", ESP.getMinFreeHeap());
@@ -17,6 +23,32 @@ extern "C" {
     M5.Lcd.printf("MaxAllocHeap: %d bytes\n", ESP.getMaxAllocHeap());
     M5.Lcd.printf("MaxAllocPsram: %d bytes\n", ESP.getMaxAllocPsram());
     M5.Lcd.printf("ChipCores: %d\n", ESP.getChipCores());
+  }
+
+  /**
+   * A dev status display 
+   */
+  void lv_dev_status_display(lv_obj_t *parent) {
+    pyp_status_label = lv_label_create(parent);
+    lv_obj_align(pyp_status_label, LV_ALIGN_TOP_LEFT, 20, 30);
+    lv_label_set_text(pyp_status_label, (String("PyPilot:              ") + preferences.getString(PYP_TCP_HOST_PREF) + String(":") + String(preferences.getInt(PYP_TCP_PORT_PREF))).c_str());
+
+    signalk_status_label = lv_label_create(parent);
+    lv_obj_align(signalk_status_label, LV_ALIGN_TOP_LEFT, 20, 60);
+    lv_label_set_text(signalk_status_label, (String("SignalK:             ") + preferences.getString(SK_TCP_HOST_PREF) + String(":") + String(preferences.getInt(SK_TCP_PORT_PREF))).c_str());
+
+    nmea0183_status_label = lv_label_create(parent);
+    lv_obj_align(nmea0183_status_label, LV_ALIGN_TOP_LEFT, 20, 90);
+    lv_label_set_text(nmea0183_status_label, (String("NMEA 0183:       ") + preferences.getString(NMEA0183_TCP_HOST_PREF) + String(":") + String(preferences.getInt(NMEA0183_TCP_PORT_PREF))).c_str());
+  }
+
+  static void dev_status_update_cb() {
+  }
+
+  void init_devStatusScreen() {
+    devStatusScreen.screen = lv_obj_create(NULL);  // Creates a Screen object
+    lv_dev_status_display(devStatusScreen.screen);
+    devStatusScreen.update_cb = dev_status_update_cb;
   }
 
 #ifdef __cplusplus
