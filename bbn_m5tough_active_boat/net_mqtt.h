6352b3ebe32a9734729535879578e16081433386
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-13 15:24:05 -0400

    Update net_mqtt.h

--- bbn_m5tough_active_boat/net_mqtt.h
+++ bbn_m5tough_active_boat/net_mqtt.h
@@ -5,7 +5,19 @@
 extern "C" {
 #endif
 
+/*
 
+  Victron MQQT
+  To start simulator wth docker https://github.com/victronenergy/venus-docker
+  
+  /simulate.sh --with-solarcharger --with-tanks p
+  
+  To see messages:
+  mosquitto_sub -t 'N/#'
+
+  See also: https://github.com/sbender9/signalk-venus-plugin/blob/master/index.js
+
+*/
 
 #ifdef __cplusplus
 } /*extern "C"*/
