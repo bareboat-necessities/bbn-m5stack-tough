f42d85b72dbd91c62a7b79d0bfca32803e4b9edb
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-13 22:57:21 -0400

    Update bbn_m5tough_mqtt_victron.ino

--- bbn_m5tough_mqtt_victron/bbn_m5tough_mqtt_victron.ino
+++ bbn_m5tough_mqtt_victron/bbn_m5tough_mqtt_victron.ino
@@ -8,7 +8,7 @@ static String wifi_ssid;      // Store the name of the wireless network.
 static String wifi_password;  // Store the password of the wireless network.
 
 static WiFiClient net;
-static MQTTClient client = MQTTClient(512);
+static MQTTClient client = MQTTClient(2048);
 
 typedef struct _victron_mqtt_context_t {
   String portalID;
