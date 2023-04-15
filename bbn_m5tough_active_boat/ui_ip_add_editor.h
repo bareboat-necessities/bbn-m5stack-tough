eadfb03ff94728cbfb993485b2016b5da4da1f17
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-15 10:52:49 -0400

    Update ui_ip_add_editor.h

--- bbn_m5tough_active_boat/ui_ip_add_editor.h
+++ bbn_m5tough_active_boat/ui_ip_add_editor.h
@@ -1,3 +1,4 @@
+#include "core/lv_obj.h"
 #ifndef UI_IP_ADD_EDITOR_H
 #define UI_IP_ADD_EDITOR_H
 
@@ -10,6 +11,48 @@ extern "C" {
   static lv_obj_t *spinbox2 = NULL;
   static lv_obj_t *spinbox3 = NULL;
   static lv_obj_t *spinbox4 = NULL;
+  static lv_obj_t *spinbox_port = NULL;
+
+  static void lv_spinbox_port_inc_ev_cb(lv_event_t *e) {
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t *spinbox = (lv_obj_t *)lv_event_get_user_data(e);
+    if (code == LV_EVENT_SHORT_CLICKED || code == LV_EVENT_LONG_PRESSED_REPEAT) {
+      lv_spinbox_increment(spinbox);
+    }
+  }
+
+  static void lv_spinbox_port_dec_ev_cb(lv_event_t *e) {
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t *spinbox = (lv_obj_t *)lv_event_get_user_data(e);
+    if (code == LV_EVENT_SHORT_CLICKED || code == LV_EVENT_LONG_PRESSED_REPEAT) {
+      lv_spinbox_decrement(spinbox);
+    }
+  }
+
+  lv_obj_t *lv_spinbox_port(lv_obj_t *parent, lv_align_t align, lv_coord_t x_ofs, lv_coord_t y_ofs) {
+    lv_obj_t *spinbox = lv_spinbox_create(parent);
+    lv_spinbox_set_range(spinbox, 0, 65535);
+    lv_spinbox_set_digit_format(spinbox, 5, 0);
+    lv_spinbox_step_prev(spinbox);
+    lv_obj_set_width(spinbox, 80);
+    lv_obj_align(spinbox, align, x_ofs, y_ofs);
+    lv_obj_set_style_text_align(spinbox, LV_TEXT_ALIGN_CENTER, 0);
+
+    lv_coord_t h = lv_obj_get_height(spinbox);
+    lv_obj_t *btn = lv_btn_create(parent);
+    lv_obj_set_size(btn, h, h);
+    lv_obj_align_to(btn, spinbox, LV_ALIGN_OUT_RIGHT_MID, 5, 0);
+    lv_obj_set_style_bg_img_src(btn, LV_SYMBOL_PLUS, 0);
+    lv_obj_add_event_cb(btn, lv_spinbox_port_inc_ev_cb, LV_EVENT_ALL, (void *)spinbox);
+
+    btn = lv_btn_create(parent);
+    lv_obj_set_size(btn, h, h);
+    lv_obj_align_to(btn, spinbox, LV_ALIGN_OUT_LEFT_MID, -5, 0);
+    lv_obj_add_event_cb(btn, lv_spinbox_port_dec_ev_cb, LV_EVENT_ALL, (void *)spinbox);
+    lv_obj_set_style_bg_img_src(btn, LV_SYMBOL_MINUS, 0);
+
+    return spinbox;
+  }
 
   static void lv_set_spinbox_val(lv_obj_t *spinbox, int32_t v) {
     int digits = 1;
@@ -73,7 +116,6 @@ extern "C" {
     lv_spinbox_set_rollover(spinbox, true);
     lv_obj_set_width(spinbox, 62);
     lv_obj_align(spinbox, align, x_ofs, y_ofs);
-    //lv_obj_set_style_bg_opa(spinbox, 0, LV_PART_CURSOR);
     lv_obj_set_style_text_align(spinbox, LV_TEXT_ALIGN_CENTER, 0);
 
     lv_coord_t h = lv_obj_get_height(spinbox);
@@ -106,20 +148,22 @@ extern "C" {
     if (spinboxes_parent == NULL) {
       spinboxes_parent = lv_obj_create(parent);
       lv_obj_set_size(spinboxes_parent, 320, 240);
-      spinbox1 = lv_spinbox_255(spinboxes_parent, LV_ALIGN_CENTER, -105, -20);
-      spinbox2 = lv_spinbox_255(spinboxes_parent, LV_ALIGN_CENTER, -35, -20);
-      spinbox3 = lv_spinbox_255(spinboxes_parent, LV_ALIGN_CENTER, 35, -20);
-      spinbox4 = lv_spinbox_255(spinboxes_parent, LV_ALIGN_CENTER, 105, -20);
+      spinbox1 = lv_spinbox_255(spinboxes_parent, LV_ALIGN_CENTER, -105, -50);
+      spinbox2 = lv_spinbox_255(spinboxes_parent, LV_ALIGN_CENTER, -35, -50);
+      spinbox3 = lv_spinbox_255(spinboxes_parent, LV_ALIGN_CENTER, 35, -50);
+      spinbox4 = lv_spinbox_255(spinboxes_parent, LV_ALIGN_CENTER, 105, -50);
+      spinbox_port = lv_spinbox_port(spinboxes_parent, LV_ALIGN_CENTER, 0, 50);
     }
 
     lv_set_spinbox_val(spinbox1, 0);
     lv_set_spinbox_val(spinbox2, 0);
     lv_set_spinbox_val(spinbox3, 0);
     lv_set_spinbox_val(spinbox4, 0);
+    lv_set_spinbox_val(spinbox_port, 0);
 
     lv_obj_t *btn_save = lv_btn_create(spinboxes_parent);
     lv_obj_add_event_cb(btn_save, save_ip_evt_handler, LV_EVENT_ALL, spinboxes_parent);
-    lv_obj_align(btn_save, LV_ALIGN_BOTTOM_RIGHT, -5, -5);
+    lv_obj_align(btn_save, LV_ALIGN_BOTTOM_RIGHT, -4, -4);
     lv_obj_t *btn_save_l = lv_label_create(btn_save);
     lv_label_set_text(btn_save_l, LV_SYMBOL_OK);
   }
