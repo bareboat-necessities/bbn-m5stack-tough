8833457f94f2c02d574d51efed87d10e6060e044
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-14 20:11:49 -0400

    Update ui_mem_cpu_net_stat.h

--- bbn_m5tough_active_boat/ui_mem_cpu_net_stat.h
+++ bbn_m5tough_active_boat/ui_mem_cpu_net_stat.h
@@ -12,6 +12,35 @@ extern "C" {
   static lv_obj_t *signalk_status_label;
   static lv_obj_t *venus_mqtt_status_label;
   static lv_obj_t *mpd_status_label;
+  static lv_obj_t *data_connections_list;
+
+  static void event_handler_data_conn(lv_event_t *e) {
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t *obj = lv_event_get_target(e);
+    if (code == LV_EVENT_CLICKED) {
+      const char *host_type = (const char *)lv_event_get_user_data(e);
+      // TODO:
+      lv_obj_add_flag(data_connections_list, LV_OBJ_FLAG_HIDDEN);
+    }
+  }
+
+  void lv_data_connections(lv_obj_t *parent) {
+    data_connections_list = lv_list_create(parent);
+    lv_obj_set_size(data_connections_list, 220, 180);
+    lv_obj_align(data_connections_list, LV_ALIGN_CENTER, 0, 17);
+
+    lv_obj_t *btn;
+    btn = lv_list_add_btn(data_connections_list, NULL, "SignalK TCP");
+    lv_obj_add_event_cb(btn, event_handler_data_conn, LV_EVENT_CLICKED, (void *)SK_TCP_HOST_PREF);
+    btn = lv_list_add_btn(data_connections_list, NULL, "PyPilot TCP");
+    lv_obj_add_event_cb(btn, event_handler_data_conn, LV_EVENT_CLICKED, (void *)PYP_TCP_HOST_PREF);
+    btn = lv_list_add_btn(data_connections_list, NULL, "NMEA 0183 TCP");
+    lv_obj_add_event_cb(btn, event_handler_data_conn, LV_EVENT_CLICKED, (void *)NMEA0183_TCP_HOST_PREF);
+    btn = lv_list_add_btn(data_connections_list, NULL, "VE Venus MQTT");
+    lv_obj_add_event_cb(btn, event_handler_data_conn, LV_EVENT_CLICKED, (void *)VENUS_MQTT_HOST_PREF);
+    btn = lv_list_add_btn(data_connections_list, NULL, "MPD Player");
+    lv_obj_add_event_cb(btn, event_handler_data_conn, LV_EVENT_CLICKED, (void *)MPD_TCP_HOST_PREF);
+  }
 
   void printDeviceStats() {
     M5.Lcd.printf("FreeHeap: %d bytes\n", ESP.getFreeHeap());
@@ -27,7 +56,7 @@ extern "C" {
     M5.Lcd.printf("ChipCores: %d\n", ESP.getChipCores());
   }
 
-  static void erase_net_conf_evt_handler(lv_event_t* e) {
+  static void erase_net_conf_evt_handler(lv_event_t *e) {
     lv_event_code_t code = lv_event_get_code(e);
     if (code == LV_EVENT_LONG_PRESSED) {
       erase_mdns_lookups();
@@ -35,9 +64,10 @@ extern "C" {
     }
   }
 
-  static void edit_net_conf_evt_handler(lv_event_t* e) {
+  static void edit_net_conf_evt_handler(lv_event_t *e) {
     lv_event_code_t code = lv_event_get_code(e);
     if (code == LV_EVENT_CLICKED) {
+      lv_obj_clear_flag(data_connections_list, LV_OBJ_FLAG_HIDDEN);
     }
   }
 
@@ -86,6 +116,9 @@ extern "C" {
     lv_obj_align(btn_edit, LV_ALIGN_CENTER, -40, 90);
     lv_obj_t *btn_edit_l = lv_label_create(btn_edit);
     lv_label_set_text(btn_edit_l, LV_SYMBOL_SETTINGS);
+
+    lv_data_connections(parent);
+    lv_obj_add_flag(data_connections_list, LV_OBJ_FLAG_HIDDEN);
   }
 
   static void dev_status_update_cb() {
