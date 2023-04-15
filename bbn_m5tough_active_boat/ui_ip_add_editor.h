ed4a85543da6b248ac7477e71c14e4c6e38224a7
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-14 22:02:23 -0400

    Update ui_ip_add_editor.h

--- bbn_m5tough_active_boat/ui_ip_add_editor.h
+++ bbn_m5tough_active_boat/ui_ip_add_editor.h
@@ -43,6 +43,35 @@ extern "C" {
     lv_obj_add_flag(spinbox4, LV_OBJ_FLAG_HIDDEN);
   }
 
+  lv_obj_t *lv_spinbox_255(lv_obj_t *parent, lv_align_t align, lv_coord_t x_ofs, lv_coord_t y_ofs) {
+    lv_obj_t *spinbox = lv_spinbox_create(parent);
+    lv_spinbox_set_range(spinbox, 0, 255);
+    lv_spinbox_set_digit_format(spinbox, 3, 0);
+    lv_spinbox_set_step(spinbox, 1);
+    lv_spinbox_set_rollover(spinbox, true);
+    lv_obj_set_width(spinbox, 62);
+    lv_obj_align(spinbox, align, x_ofs, y_ofs);
+    lv_obj_set_style_bg_opa(spinbox, 0, LV_PART_CURSOR);
+    lv_obj_set_style_text_align(spinbox, LV_TEXT_ALIGN_CENTER, 0);
+
+    lv_coord_t h = lv_obj_get_height(spinbox);
+
+    lv_obj_t *btn = lv_btn_create(parent);
+    lv_obj_set_size(btn, h, h);
+    lv_obj_align_to(btn, spinbox, LV_ALIGN_OUT_TOP_MID, 0, -10);
+    lv_obj_set_style_bg_img_src(btn, LV_SYMBOL_PLUS, 0);
+    lv_obj_add_event_cb(btn, lv_spinbox_255_inc_ev_cb, LV_EVENT_ALL, (void *)spinbox);
+
+    btn = lv_btn_create(parent);
+    lv_obj_set_size(btn, h, h);
+    lv_obj_align_to(btn, spinbox, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
+    lv_obj_set_style_bg_img_src(btn, LV_SYMBOL_MINUS, 0);
+    lv_obj_add_event_cb(btn, lv_spinbox_255_dec_ev_cb, LV_EVENT_ALL, (void *)spinbox);
+
+    lv_set_spinbox_val(spinbox, 0);
+    return spinbox;
+  }
+
   void lv_ip_addr_editor(lv_obj_t *parent, const char *addr) {
     spinbox1 = lv_spinbox_255(parent, LV_ALIGN_CENTER, -105, 0);
     spinbox2 = lv_spinbox_255(parent, LV_ALIGN_CENTER, -35, 0);
@@ -80,37 +109,6 @@ extern "C" {
       }
     }
   }
-
-  lv_obj_t *lv_spinbox_255(lv_obj_t *parent, lv_align_t align, lv_coord_t x_ofs, lv_coord_t y_ofs) {
-    lv_obj_t *spinbox = lv_spinbox_create(parent);
-    lv_spinbox_set_range(spinbox, 0, 255);
-    lv_spinbox_set_digit_format(spinbox, 3, 0);
-    lv_spinbox_set_step(spinbox, 1);
-    lv_spinbox_set_rollover(spinbox, true);
-    lv_obj_set_width(spinbox, 62);
-    lv_obj_align(spinbox, align, x_ofs, y_ofs);
-    lv_obj_set_style_bg_opa(spinbox, 0, LV_PART_CURSOR);
-    lv_obj_set_style_text_align(spinbox, LV_TEXT_ALIGN_CENTER, 0);
-
-    lv_coord_t h = lv_obj_get_height(spinbox);
-
-    lv_obj_t *btn = lv_btn_create(parent);
-    lv_obj_set_size(btn, h, h);
-    lv_obj_align_to(btn, spinbox, LV_ALIGN_OUT_TOP_MID, 0, -10);
-    lv_obj_set_style_bg_img_src(btn, LV_SYMBOL_PLUS, 0);
-    lv_obj_add_event_cb(btn, lv_spinbox_255_inc_ev_cb, LV_EVENT_ALL, (void *)spinbox);
-
-    btn = lv_btn_create(parent);
-    lv_obj_set_size(btn, h, h);
-    lv_obj_align_to(btn, spinbox, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
-    lv_obj_set_style_bg_img_src(btn, LV_SYMBOL_MINUS, 0);
-    lv_obj_add_event_cb(btn, lv_spinbox_255_dec_ev_cb, LV_EVENT_ALL, (void *)spinbox);
-
-    lv_set_spinbox_val(spinbox, 0);
-    return spinbox;
-  }
-
-
 
 #ifdef __cplusplus
 } /*extern "C"*/
