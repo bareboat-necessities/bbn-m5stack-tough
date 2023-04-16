a011904d1bc14ae4f6e46833d66a7f6fadf50a98
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-15 21:00:17 -0400

    Update net_mpd_tcp.h

--- bbn_m5tough_active_boat/net_mpd_tcp.h
+++ bbn_m5tough_active_boat/net_mpd_tcp.h
@@ -48,4 +48,79 @@ idle
 
 https://mpd.readthedocs.io/en/latest/protocol.html
 
+
+telnet lysmarine 6600
+Trying 192.168.1.34...
+Connected to lysmarine.fios-router.home.
+Escape character is '^]'.
+OK MPD 0.19.0
+listplaylists
+playlist: BBN-Playlist
+Last-Modified: 2023-04-16T00:57:59Z
+playlist: [Radio Streams]
+Last-Modified: 2023-04-16T00:57:59Z
+OK
+load BBN-Playlist 0
+OK
+playlistinfo
+file: youtube:video:hOG-y6zsjzw
+Time: 205
+Artist: Willy William (Channel)
+Album: YouTube Playlist
+Title: Le tour du monde (feat. Natty Rico, Mika V)
+Pos: 0
+Id: 1
+file: youtube:video:PDQYYPRQ5OM
+Time: 0
+Artist: MaksCVMusic
+Album: YouTube Playlist
+Title: Sublime - Santeria [Lyrics Sub Español/English]
+Pos: 1
+Id: 2
+file: youtube:video:aeQBKa0AAXs
+Time: 201
+Artist: Michael Angelo (Channel)
+Album: YouTube Playlist
+Title: Celia Cruz - Rie y Llora (2003)
+Pos: 2
+Id: 3
+file: youtube:video:Beo_jHowU-I
+Time: 139
+Artist: murilopolla (Channel)
+Album: YouTube Playlist
+Title: 16 Toneladas - Noriel Vilela
+Pos: 3
+Id: 4
+file: youtube:video:74LXx0wSqMI
+Time: 232
+Artist: caroemerald (Channel)
+Album: YouTube Playlist
+Title: Caro Emerald - A Night Like This (Official Video)
+Pos: 4
+Id: 5
+file: youtube:video:ROvrIiASqJU
+Time: 248
+Artist: World Circuit Records (Channel)
+Album: YouTube Playlist
+Title: Buena Vista Social Club - Lágrimas Negras - feat. Omara Portuondo (Official Audio)
+Pos: 5
+Id: 6
+file: youtube:video:hOG-y6zsjzw
+Time: 205
+Artist: Willy William (Channel)
+Album: YouTube Playlist
+Title: Le tour du monde (feat. Natty Rico, Mika V)
+Pos: 6
+Id: 7
+file: youtube:video:hOG-y6zsjzw
+Time: 205
+Artist: Willy William (Channel)
+Album: YouTube Playlist
+Title: Le tour du monde (feat. Natty Rico, Mika V)
+Pos: 7
+Id: 8
+OK
+play 0
+OK
+
 */
