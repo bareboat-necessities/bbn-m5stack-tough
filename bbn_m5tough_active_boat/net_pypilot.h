ccd3bf4f78ec12a5899ba6fd5c9b7c9f5a23129c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-15 21:55:56 -0400

    Update net_pypilot.h

--- bbn_m5tough_active_boat/net_pypilot.h
+++ bbn_m5tough_active_boat/net_pypilot.h
@@ -137,12 +137,12 @@ extern "C" {
     setKeepAlive(pypClient);
     setup_pypilot_reconnect(pypClient, pyp_host, pyp_port);
     if (pypClient.connect(pyp_host, pyp_port)) {
-      M5.Lcd.print("Connected to pypilot ");
-      M5.Lcd.println(pyp_host);
+      //M5.Lcd.print("Connected to pypilot ");
+      //M5.Lcd.println(pyp_host);
       pypilot_subscribe(pypClient);
-    } else {
-      M5.Lcd.println("Connection failed.");
-    }
+    } //else {
+      //M5.Lcd.println("Connection failed.");
+    //}
   }
 
 #ifdef __cplusplus
