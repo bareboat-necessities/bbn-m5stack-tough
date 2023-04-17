b08b9a7cdd87dd5109752527d59c5122e2fa719c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-17 16:26:17 -0400

    Update ui_weather.h

--- bbn_m5tough_active_boat/ui_weather.h
+++ bbn_m5tough_active_boat/ui_weather.h
@@ -17,9 +17,11 @@ extern "C" {
   lv_updatable_screen_t weatherScreen;
 
   static lv_obj_t *weather_time_label;
-  static lv_obj_t *weather_sunrise_label;
-  static lv_obj_t *weather_sunset_label;
   static lv_obj_t *weather_twilight_begin_label;
+  static lv_obj_t *weather_sunrise_label;
+  static lv_obj_t *weather_daylight_label;
+  static lv_obj_t *weather_sunset_label;
+  static lv_obj_t *weather_twilight_label;
   static lv_obj_t *weather_twilight_end_label;
 
   /**
@@ -32,35 +34,49 @@ extern "C" {
     lv_label_set_text(main_label, "Weather  " LV_SYMBOL_IMAGE);
 
     weather_time_label = lv_label_create(parent);
-    lv_obj_align(weather_time_label, LV_ALIGN_TOP_LEFT, 10, 40);
+    lv_obj_align(weather_time_label, LV_ALIGN_TOP_LEFT, 10, 30);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(weather_time_label, &lv_font_montserrat_20, 0);
 #endif
     lv_label_set_text(weather_time_label, "UTC Time:         --");
 
     weather_twilight_begin_label = lv_label_create(parent);
-    lv_obj_align(weather_twilight_begin_label, LV_ALIGN_TOP_LEFT, 10, 70);
+    lv_obj_align(weather_twilight_begin_label, LV_ALIGN_TOP_LEFT, 10, 60);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(weather_twilight_begin_label, &lv_font_montserrat_20, 0);
 #endif
     lv_label_set_text(weather_twilight_begin_label, "Twilight Start: ---");
 
     weather_sunrise_label = lv_label_create(parent);
-    lv_obj_align(weather_sunrise_label, LV_ALIGN_TOP_LEFT, 10, 100);
+    lv_obj_align(weather_sunrise_label, LV_ALIGN_TOP_LEFT, 10, 90);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(weather_sunrise_label, &lv_font_montserrat_20, 0);
 #endif
     lv_label_set_text(weather_sunrise_label, "Sunrise:          --");
 
+    weather_daylight_label = lv_label_create(parent);
+    lv_obj_align(weather_daylight_label, LV_ALIGN_TOP_LEFT, 10, 120);
+#if LV_FONT_MONTSERRAT_20
+    lv_obj_set_style_text_font(weather_daylight_label, &lv_font_montserrat_20, 0);
+#endif
+    lv_label_set_text(weather_daylight_label, "Day Duration:   ---");
+
     weather_sunset_label = lv_label_create(parent);
-    lv_obj_align(weather_sunset_label, LV_ALIGN_TOP_LEFT, 10, 130);
+    lv_obj_align(weather_sunset_label, LV_ALIGN_TOP_LEFT, 10, 150);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(weather_sunset_label, &lv_font_montserrat_20, 0);
 #endif
     lv_label_set_text(weather_sunset_label, "Sunset:          --");
 
+    weather_twilight_label = lv_label_create(parent);
+    lv_obj_align(weather_twilight_label, LV_ALIGN_TOP_LEFT, 10, 180);
+#if LV_FONT_MONTSERRAT_20
+    lv_obj_set_style_text_font(weather_twilight_label, &lv_font_montserrat_20, 0);
+#endif
+    lv_label_set_text(weather_twilight_label, "Twilight Duration:   ---");
+
     weather_twilight_end_label = lv_label_create(parent);
-    lv_obj_align(weather_twilight_end_label, LV_ALIGN_TOP_LEFT, 10, 160);
+    lv_obj_align(weather_twilight_end_label, LV_ALIGN_TOP_LEFT, 10, 210);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(weather_twilight_end_label, &lv_font_montserrat_20, 0);
 #endif
@@ -78,23 +94,31 @@ extern "C" {
 
   static void weather_update_cb() {
     lv_label_set_text(weather_time_label,
-                      (String("UTC Time:          ")
+                      (String("UTC Time:                   ")
                        + (fresh(shipDataModel.environment.time_gps.age) ? time_format(shipDataModel.environment.time_gps.t) : String("--")))
                         .c_str());
     lv_label_set_text(weather_twilight_begin_label,
-                      (String("Twilight Start:  ")
+                      (String("Twilight Start:           ")
                        + (fresh(shipDataModel.environment.nautical_twilight_start.age, TWO_MINUTES) ? hour_format(shipDataModel.environment.nautical_twilight_start.hr) : String("--")))
                         .c_str());
     lv_label_set_text(weather_sunrise_label,
-                      (String("Sunrise:               ")
+                      (String("Sunrise:                       ")
                        + (fresh(shipDataModel.environment.sunrise.age, TWO_MINUTES) ? hour_format(shipDataModel.environment.sunrise.hr) : String("--")))
                         .c_str());
+    lv_label_set_text(weather_daylight_label,
+                      (String("Day Duration:           ")
+                       + (fresh(shipDataModel.environment.daylight_duration.age, TWO_MINUTES) ? hour_format(shipDataModel.environment.daylight_duration.hr) : String("--")))
+                        .c_str());
     lv_label_set_text(weather_sunset_label,
-                      (String("Sunset:                ")
+                      (String("Sunset:                        ")
                        + (fresh(shipDataModel.environment.sunset.age, TWO_MINUTES) ? hour_format(shipDataModel.environment.sunset.hr) : String("--")))
                         .c_str());
+    lv_label_set_text(weather_twilight_label,
+                      (String("Twilight Duration:  ")
+                       + (fresh(shipDataModel.environment.nautical_twilight_duration.age, TWO_MINUTES) ? hour_format(shipDataModel.environment.nautical_twilight_duration.hr) : String("--")))
+                        .c_str());
     lv_label_set_text(weather_twilight_end_label,
-                      (String("Twilight End:    ")
+                      (String("Twilight End:            ")
                        + (fresh(shipDataModel.environment.nautical_twilight_end.age, TWO_MINUTES) ? hour_format(shipDataModel.environment.nautical_twilight_end.hr) : String("--")))
                         .c_str());
   }
