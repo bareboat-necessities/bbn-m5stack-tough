bdb354741526da4098deb8b042efb5d153b38d0f
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-17 20:02:17 -0400

    Update ui_screens.h

--- bbn_m5tough_active_boat/ui_screens.h
+++ bbn_m5tough_active_boat/ui_screens.h
@@ -11,10 +11,13 @@ extern "C" {
   static void noop_update_cb() {
   }
 
+  static void noop_init_cb(lv_obj_t* parent) {
+  }
+
   typedef struct _lv_updatable_screen_t {
     lv_obj_t* screen;
     lv_update_screen_data_cb_t update_cb = noop_update_cb;  // Default NoOp callback
-    lv_init_screen_cb_t init_cb;
+    lv_init_screen_cb_t init_cb = noop_init_cb;
   } lv_updatable_screen_t;
 
   // triggers callback function set in update_cb
