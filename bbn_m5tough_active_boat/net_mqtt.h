55856010a89da1447442d34588a9855fc3ca47e1
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-18 23:59:37 -0400

    Update net_mqtt.h

--- bbn_m5tough_active_boat/net_mqtt.h
+++ bbn_m5tough_active_boat/net_mqtt.h
@@ -25,7 +25,27 @@ extern "C" {
     unsigned long lastMillis = 0;
   } victron_mqtt_context_t;
 
+  typedef enum {
+    FLUID_TYPE_NA = -1,
+    FUEL = 0,
+    FRESH_WATER = 1,
+    WASTE_WATER = 2,
+    LIVE_WELL = 3,
+    LUBRICATION = 4,
+    BLACK_WATER = 5
+  } fluid_type_e;
+
   static victron_mqtt_context_t victronCtx;
+
+  bool starts_with(const char* str, const char* pre) {
+    return strncmp(pre, str, strlen(pre)) == 0;
+  }
+
+  const char* step_into_path(const char* path) {
+    if (path == NULL) return NULL;
+    const char* s = strchr(path, '/');
+    return s != NULL ? s++ : NULL;
+  }
 
   static void victron_mqtt_on_message(String& topic, String& payload) {
     if (topic.endsWith("/system/0/Serial") && victronCtx.needs_id) {
@@ -34,6 +54,29 @@ extern "C" {
       victronCtx.portalID.clear();
       victronCtx.portalID += str;
       victronCtx.needs_id = false;
+    } else {
+      const char* lev1 = step_into_path(topic.c_str());
+      const char* lev2 = step_into_path(lev1);
+      const char* lev3 = step_into_path(lev2);
+      const char* lev4 = step_into_path(lev3);
+      const char* lev5 = step_into_path(lev4);
+
+      if (lev5 != NULL) {
+        if (starts_with(lev3, "tank/")) {
+          int num = atoi(lev4);
+          if (strcmp(lev5, "FluidType") == 0) {
+            // TODO:
+          } else if (strcmp(lev5, "Remaining") == 0) {
+            // TODO: %
+          }
+        }
+      }
+
+      // topic:    N/0242ac110002/tank/3/FluidType
+      // payload:  {"value": 5}
+
+      // topic:    N/0242ac110002/tank/3/Remaining
+      // payload:  {"value": 0.18332000076770782}
     }
 
     // Note: Do not use the client in the callback to publish, subscribe or
