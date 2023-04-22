8fbb4ec99eb8bbb1ae723f7edd129227b5276d49
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-22 08:53:48 -0400

    Update ui_tanks.h

--- bbn_m5tough_active_boat/ui_tanks.h
+++ bbn_m5tough_active_boat/ui_tanks.h
@@ -20,7 +20,7 @@ extern "C" {
       lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_GREY, 3), LV_PART_MAIN);
       lv_obj_set_style_radius(bar_tank[i], 0, LV_PART_INDICATOR);
       lv_bar_set_range(bar_tank[i], 0, 100);
-      bar_tank_l[i] = lv_label_create(parent);      
+      bar_tank_l[i] = lv_label_create(parent);
     }
 
     lv_obj_align(bar_tank[0], LV_ALIGN_CENTER, -120, -40);
@@ -45,35 +45,37 @@ extern "C" {
   static void tanks_update_cb() {
     for (int i = 0; i < MAX_TANKS; i++) {
       float pct = shipDataModel.tanks.tank[i].percent_of_full.pct;
-      lv_bar_set_value(bar_tank[i], pct, LV_ANIM_OFF);
-      if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::FUEL) {
-        lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_GREEN, 1), LV_PART_INDICATOR);
-        lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
-        lv_label_set_text(bar_tank_l[i], (String("Fuel\n") + String(pct, 1) + "%").c_str());
-      } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::BLACK_WATER) {
-        lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_RED, 1), LV_PART_INDICATOR);
-        lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
-        lv_label_set_text(bar_tank_l[i], (String("Black\n") + String(pct, 1) + "%").c_str());
-      } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::WASTE_WATER) {
-        lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_RED, 1), LV_PART_INDICATOR);
-        lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
-        lv_label_set_text(bar_tank_l[i], (String("Grey\n") + String(pct, 1) + "%").c_str());
-      } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::FRESH_WATER) {
-        lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
-        lv_label_set_text(bar_tank_l[i], (String("Fresh\n") + String(pct, 1) + "%").c_str());
-      } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::LUBRICATION) {
-        lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
-        lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_ORANGE, 1), LV_PART_INDICATOR);
-        lv_label_set_text(bar_tank_l[i], (String("Lube\n") + String(pct, 1) + "%").c_str());
-      } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::LIVE_WELL) {
-        lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
-        lv_label_set_text(bar_tank_l[i], (String("Live Well\n") + String(pct, 1) + "%").c_str());
-      } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::GAS) {
-        lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_ORANGE, 1), LV_PART_INDICATOR);
-        lv_label_set_text(bar_tank_l[i], (String("Gas\n") + String(pct, 1) + "%").c_str());
-      } else {
-        lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
-        lv_label_set_text(bar_tank_l[i], "n/a");
+      if (fresh(shipDataModel.tanks.tank[i].percent_of_full.age, TWO_MINUTES)) {
+        lv_bar_set_value(bar_tank[i], pct, LV_ANIM_OFF);
+        if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::FUEL) {
+          lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_GREEN, 1), LV_PART_INDICATOR);
+          lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
+          lv_label_set_text(bar_tank_l[i], (String("Fuel\n") + String(pct, 1) + "%").c_str());
+        } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::BLACK_WATER) {
+          lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_RED, 1), LV_PART_INDICATOR);
+          lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
+          lv_label_set_text(bar_tank_l[i], (String("Black\n") + String(pct, 1) + "%").c_str());
+        } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::WASTE_WATER) {
+          lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_RED, 1), LV_PART_INDICATOR);
+          lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
+          lv_label_set_text(bar_tank_l[i], (String("Grey\n") + String(pct, 1) + "%").c_str());
+        } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::FRESH_WATER) {
+          lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
+          lv_label_set_text(bar_tank_l[i], (String("Fresh\n") + String(pct, 1) + "%").c_str());
+        } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::LUBRICATION) {
+          lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
+          lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_ORANGE, 1), LV_PART_INDICATOR);
+          lv_label_set_text(bar_tank_l[i], (String("Lube\n") + String(pct, 1) + "%").c_str());
+        } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::LIVE_WELL) {
+          lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
+          lv_label_set_text(bar_tank_l[i], (String("Live Well\n") + String(pct, 1) + "%").c_str());
+        } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::GAS) {
+          lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_ORANGE, 1), LV_PART_INDICATOR);
+          lv_label_set_text(bar_tank_l[i], (String("Gas\n") + String(pct, 1) + "%").c_str());
+        } else {
+          lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
+          lv_label_set_text(bar_tank_l[i], "n/a");
+        }
       }
     }
   }
