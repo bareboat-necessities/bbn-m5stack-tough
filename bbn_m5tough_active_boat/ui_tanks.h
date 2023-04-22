30d5cf9c0389c04e1d63d9d0caea12ebf3b0db21
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-21 23:02:35 -0400

    Update ui_tanks.h

--- bbn_m5tough_active_boat/ui_tanks.h
+++ bbn_m5tough_active_boat/ui_tanks.h
@@ -7,129 +7,74 @@ extern "C" {
 
   lv_updatable_screen_t tanksScreen;
 
-  static lv_obj_t *bar_w1;
-  static lv_obj_t *bar_w2;
-  static lv_obj_t *bar_bw1;
-  static lv_obj_t *bar_bw2;
-  static lv_obj_t *bar_f1;
-  static lv_obj_t *bar_f2;
-  static lv_obj_t *bar_g1;
-  static lv_obj_t *bar_g2;
+  static lv_obj_t *bar_tank[MAX_TANKS];
+  static lv_obj_t *bar_tank_l[MAX_TANKS];
 
   /**
    * A tanks display 
    */
   static void lv_tanks_display(lv_obj_t *parent) {
-    bar_w1 = lv_bar_create(parent);
-    lv_obj_set_size(bar_w1, 50, 70);
-    lv_obj_set_style_bg_color(bar_w1, lv_palette_lighten(LV_PALETTE_GREY, 3), LV_PART_MAIN);
-    lv_obj_set_style_radius(bar_w1, 6, LV_PART_MAIN);
-    lv_obj_set_style_radius(bar_w1, 0, LV_PART_INDICATOR);
-    lv_obj_align(bar_w1, LV_ALIGN_CENTER, -120, -40);
-    lv_bar_set_range(bar_w1, 0, 100);
+    for (int i = 0; i < MAX_TANKS; i++) {
+      bar_tank[i] = lv_bar_create(parent);
+      lv_obj_set_size(bar_tank[i], 50, 70);
+      lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_GREY, 3), LV_PART_MAIN);
+      lv_obj_set_style_radius(bar_tank[i], 0, LV_PART_INDICATOR);
+      lv_bar_set_range(bar_tank[i], 0, 100);
+      bar_tank_l[i] = lv_label_create(parent);      
+    }
 
-    lv_obj_t *bar_w1_l = lv_label_create(parent);
-    lv_obj_align(bar_w1_l, LV_ALIGN_CENTER, -120, -40 - 60);
-    lv_label_set_text(bar_w1_l, "Fresh 1\n--%");
+    lv_obj_align(bar_tank[0], LV_ALIGN_CENTER, -120, -40);
+    lv_obj_align(bar_tank[1], LV_ALIGN_CENTER, -40, -40);
+    lv_obj_align(bar_tank[2], LV_ALIGN_CENTER, 40, -40);
+    lv_obj_align(bar_tank[3], LV_ALIGN_CENTER, 120, -40);
+    lv_obj_align(bar_tank[4], LV_ALIGN_CENTER, -120, 40);
+    lv_obj_align(bar_tank[5], LV_ALIGN_CENTER, -40, 40);
+    lv_obj_align(bar_tank[6], LV_ALIGN_CENTER, 40, 40);
+    lv_obj_align(bar_tank[7], LV_ALIGN_CENTER, 120, 40);
 
-    bar_w2 = lv_bar_create(parent);
-    lv_obj_set_size(bar_w2, 50, 70);
-    lv_obj_set_style_bg_color(bar_w2, lv_palette_lighten(LV_PALETTE_GREY, 3), LV_PART_MAIN);
-    lv_obj_set_style_radius(bar_w2, 6, LV_PART_MAIN);
-    lv_obj_set_style_radius(bar_w2, 0, LV_PART_INDICATOR);
-    lv_obj_align(bar_w2, LV_ALIGN_CENTER, -40, -40);
-    lv_bar_set_range(bar_w2, 0, 100);
-
-    lv_obj_t *bar_w2_l = lv_label_create(parent);
-    lv_obj_align(bar_w2_l, LV_ALIGN_CENTER, -40, -40 - 60);
-    lv_label_set_text(bar_w2_l, "Fresh 2\n--%");
-
-    bar_bw1 = lv_bar_create(parent);
-    lv_obj_set_size(bar_bw1, 50, 70);
-    lv_obj_set_style_bg_color(bar_bw1, lv_palette_lighten(LV_PALETTE_GREY, 3), LV_PART_MAIN);
-    lv_obj_set_style_bg_color(bar_bw1, lv_palette_lighten(LV_PALETTE_RED, 1), LV_PART_INDICATOR);
-    lv_obj_set_style_radius(bar_bw1, 6, LV_PART_MAIN);
-    lv_obj_set_style_radius(bar_bw1, 0, LV_PART_INDICATOR);
-    lv_obj_align(bar_bw1, LV_ALIGN_CENTER, 40, -40);
-    lv_bar_set_range(bar_bw1, 0, 100);
-
-    lv_obj_t *bar_bw1_l = lv_label_create(parent);
-    lv_obj_align(bar_bw1_l, LV_ALIGN_CENTER, 40, -40 - 60);
-    lv_label_set_text(bar_bw1_l, "Black 1\n--%");
-
-    bar_bw2 = lv_bar_create(parent);
-    lv_obj_set_size(bar_bw2, 50, 70);
-    lv_obj_set_style_bg_color(bar_bw2, lv_palette_lighten(LV_PALETTE_GREY, 3), LV_PART_MAIN);
-    lv_obj_set_style_bg_color(bar_bw2, lv_palette_lighten(LV_PALETTE_RED, 1), LV_PART_INDICATOR);
-    lv_obj_set_style_radius(bar_bw2, 6, LV_PART_MAIN);
-    lv_obj_set_style_radius(bar_bw2, 0, LV_PART_INDICATOR);
-    lv_obj_align(bar_bw2, LV_ALIGN_CENTER, 120, -40);
-    lv_bar_set_range(bar_bw2, 0, 100);
-
-    lv_obj_t *bar_bw2_l = lv_label_create(parent);
-    lv_obj_align(bar_bw2_l, LV_ALIGN_CENTER, 120, -40 - 60);
-    lv_label_set_text(bar_bw2_l, "Black 2\n--%");
-
-    bar_f1 = lv_bar_create(parent);
-    lv_obj_set_size(bar_f1, 50, 70);
-    lv_obj_set_style_bg_color(bar_f1, lv_palette_lighten(LV_PALETTE_GREY, 3), LV_PART_MAIN);
-    lv_obj_set_style_bg_color(bar_f1, lv_palette_lighten(LV_PALETTE_GREEN, 1), LV_PART_INDICATOR);
-    lv_obj_set_style_radius(bar_f1, 6, LV_PART_MAIN);
-    lv_obj_set_style_radius(bar_f1, 0, LV_PART_INDICATOR);
-    lv_obj_align(bar_f1, LV_ALIGN_CENTER, -120, 40);
-    lv_bar_set_range(bar_f1, 0, 100);
-
-    lv_obj_t *bar_f1_l = lv_label_create(parent);
-    lv_obj_align(bar_f1_l, LV_ALIGN_CENTER, -120, 40 + 60);
-    lv_label_set_text(bar_f1_l, "Fuel 1\n--%");
-
-    bar_f2 = lv_bar_create(parent);
-    lv_obj_set_size(bar_f2, 50, 70);
-    lv_obj_set_style_bg_color(bar_f2, lv_palette_lighten(LV_PALETTE_GREY, 3), LV_PART_MAIN);
-    lv_obj_set_style_bg_color(bar_f2, lv_palette_lighten(LV_PALETTE_GREEN, 1), LV_PART_INDICATOR);
-    lv_obj_set_style_radius(bar_f2, 6, LV_PART_MAIN);
-    lv_obj_set_style_radius(bar_f2, 0, LV_PART_INDICATOR);
-    lv_obj_align(bar_f2, LV_ALIGN_CENTER, -40, 40);
-    lv_bar_set_range(bar_f2, 0, 100);
-
-    lv_obj_t *bar_f2_l = lv_label_create(parent);
-    lv_obj_align(bar_f2_l, LV_ALIGN_CENTER, -40, 40 + 60);
-    lv_label_set_text(bar_f2_l, "Fuel 2\n--%");
-
-    bar_g1 = lv_bar_create(parent);
-    lv_obj_set_size(bar_g1, 50, 70);
-    lv_obj_set_style_bg_color(bar_g1, lv_palette_lighten(LV_PALETTE_GREY, 3), LV_PART_MAIN);
-    lv_obj_set_style_bg_color(bar_g1, lv_palette_lighten(LV_PALETTE_ORANGE, 1), LV_PART_INDICATOR);
-    lv_obj_set_style_radius(bar_g1, 0, LV_PART_INDICATOR);
-    lv_obj_align(bar_g1, LV_ALIGN_CENTER, 40, 40);
-    lv_bar_set_range(bar_g1, 0, 100);
-
-    lv_obj_t *bar_g1_l = lv_label_create(parent);
-    lv_obj_align(bar_g1_l, LV_ALIGN_CENTER, 40, 40 + 60);
-    lv_label_set_text(bar_g1_l, "Gas 1\n--%");
-
-    bar_g2 = lv_bar_create(parent);
-    lv_obj_set_size(bar_g2, 50, 70);
-    lv_obj_set_style_bg_color(bar_g2, lv_palette_lighten(LV_PALETTE_GREY, 3), LV_PART_MAIN);
-    lv_obj_set_style_bg_color(bar_g2, lv_palette_lighten(LV_PALETTE_ORANGE, 1), LV_PART_INDICATOR);
-    lv_obj_set_style_radius(bar_g2, 0, LV_PART_INDICATOR);
-    lv_obj_align(bar_g2, LV_ALIGN_CENTER, 120, 40);
-    lv_bar_set_range(bar_g2, 0, 100);
-
-    lv_obj_t *bar_g2_l = lv_label_create(parent);
-    lv_obj_align(bar_g2_l, LV_ALIGN_CENTER, 120, 40 + 60);
-    lv_label_set_text(bar_g2_l, "Gas 2\n--%");
+    lv_obj_align(bar_tank_l[0], LV_ALIGN_CENTER, -120, -40 - 60);
+    lv_obj_align(bar_tank_l[1], LV_ALIGN_CENTER, -40, -40 - 60);
+    lv_obj_align(bar_tank_l[2], LV_ALIGN_CENTER, 40, -40 - 60);
+    lv_obj_align(bar_tank_l[3], LV_ALIGN_CENTER, 120, -40 - 60);
+    lv_obj_align(bar_tank_l[4], LV_ALIGN_CENTER, -120, 40 + 60);
+    lv_obj_align(bar_tank_l[5], LV_ALIGN_CENTER, -40, 40 + 60);
+    lv_obj_align(bar_tank_l[6], LV_ALIGN_CENTER, 40, 40 + 60);
+    lv_obj_align(bar_tank_l[7], LV_ALIGN_CENTER, 120, 40 + 60);
   }
 
   static void tanks_update_cb() {
-    lv_bar_set_value(bar_w1, shipDataModel.tanks.tank[0].percent_of_full.pct, LV_ANIM_OFF);
-    lv_bar_set_value(bar_w2, shipDataModel.tanks.tank[1].percent_of_full.pct, LV_ANIM_OFF);
-    lv_bar_set_value(bar_bw1, shipDataModel.tanks.tank[2].percent_of_full.pct, LV_ANIM_OFF);
-    lv_bar_set_value(bar_bw2, shipDataModel.tanks.tank[3].percent_of_full.pct, LV_ANIM_OFF);
-    lv_bar_set_value(bar_f1, shipDataModel.tanks.tank[4].percent_of_full.pct, LV_ANIM_OFF);
-    lv_bar_set_value(bar_f2, shipDataModel.tanks.tank[5].percent_of_full.pct, LV_ANIM_OFF);
-    lv_bar_set_value(bar_g1, shipDataModel.tanks.tank[6].percent_of_full.pct, LV_ANIM_OFF);
-    lv_bar_set_value(bar_g2, shipDataModel.tanks.tank[7].percent_of_full.pct, LV_ANIM_OFF);
+    for (int i = 0; i < MAX_TANKS; i++) {
+      lv_bar_set_value(bar_tank[i], shipDataModel.tanks.tank[i].percent_of_full.pct, LV_ANIM_OFF);
+      if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::FUEL) {
+        lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_GREEN, 1), LV_PART_INDICATOR);
+        lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
+        lv_label_set_text(bar_tank_l[i], "Fuel\n--%");
+      } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::BLACK_WATER) {
+        lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_RED, 1), LV_PART_INDICATOR);
+        lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
+        lv_label_set_text(bar_tank_l[i], "Black\n--%");
+      } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::WASTE_WATER) {
+        lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_RED, 1), LV_PART_INDICATOR);
+        lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
+        lv_label_set_text(bar_tank_l[i], "Grey\n--%");
+      } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::FRESH_WATER) {
+        lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
+        lv_label_set_text(bar_tank_l[i], "Fresh\n--%");
+      } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::LUBRICATION) {
+        lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
+        lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_ORANGE, 1), LV_PART_INDICATOR);
+        lv_label_set_text(bar_tank_l[i], "Lube\n--%");
+      } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::LIVE_WELL) {
+        lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
+        lv_label_set_text(bar_tank_l[i], "Live Well\n--%");
+      } else if (shipDataModel.tanks.tank[i].fluid_type == fluid_type_e::GAS) {
+        lv_obj_set_style_bg_color(bar_tank[i], lv_palette_lighten(LV_PALETTE_ORANGE, 1), LV_PART_INDICATOR);
+        lv_label_set_text(bar_tank_l[i], "Gas\n--%");
+      } else {
+        lv_obj_set_style_radius(bar_tank[i], 6, LV_PART_MAIN);
+        lv_label_set_text(bar_tank_l[i], "n/a");
+      }
+    }
   }
 
   void init_tanksScreen() {
