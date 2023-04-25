9fa6cd532691dca8fe6cc0494558af6c5c9749dc
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-25 00:20:26 -0400

    Update net_nmea0183_tcp.h

--- bbn_m5tough_active_boat/net_nmea0183_tcp.h
+++ bbn_m5tough_active_boat/net_nmea0183_tcp.h
@@ -5,32 +5,37 @@
 extern "C" {
 #endif
 
-  void setup_nmea0183_reconnect(WiFiClient& client, const char* host, int port) {
+  void setup_nmea0183_reconnect(NetClient& client, const char* host, int port) {
     app.onRepeat(7000, [&client, host, port]() {
-      if (!client.connected()) {
-        setKeepAlive(client);
-        if (client.connect(host, port, 200)) {
+      if (!client.c.connected()) {
+        setKeepAlive(client.c);
+        if (client.c.connect(host, port, 200)) {
         }
+      }
+      if (client.lastActivity > 0 && (millis() - client.lastActivity) > 10000) {
+        client.c.stop();
+        client.lastActivity = 0;
       }
     });
   }
 
-  void nmea0183_subscribe(WiFiClient& client) {
-    app.onAvailable(client, [&client]() {
-      while (client.connected() && client.available() > 128 /* Very important for performance and responsiveness */) {
-        bool found = nmea0183_parse(client);
+  void nmea0183_subscribe(NetClient& client) {
+    app.onAvailable(client.c, [&client]() {
+      while (client.c.connected() && client.c.available() > 128 /* Very important for performance and responsiveness */) {
+        bool found = nmea0183_parse(client.c);
         if (found) {
+          client.lastActivity = millis();
           break; /* Very important for performance and responsiveness */
         }
       }
     });
   }
 
-  void nmea0183_tcp_begin(WiFiClient& nmea0183Client, const char* nmea0183_host, int nmea0183_port) {
-    setKeepAlive(nmea0183Client);
+  void nmea0183_tcp_begin(NetClient& nmea0183Client, const char* nmea0183_host, int nmea0183_port) {
+    setKeepAlive(nmea0183Client.c);
     setup_nmea0183_reconnect(nmea0183Client, nmea0183_host, nmea0183_port);
     nmea0183_subscribe(nmea0183Client);
-    if (nmea0183Client.connect(nmea0183_host, nmea0183_port, 300)) {}
+    if (nmea0183Client.c.connect(nmea0183_host, nmea0183_port, 300)) {}
   }
 
 #ifdef __cplusplus
