2c5989b2a1caa181ed30a73682d27de292ea7a71
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-04-28 15:37:39 -0400

    Update ui_brightness.h

--- bbn_m5tough_active_boat/ui_brightness.h
+++ bbn_m5tough_active_boat/ui_brightness.h
@@ -5,18 +5,15 @@
 extern "C" {
 #endif
 
-  int lcdVoltage = 2900;
+#define MAX_LCD_V 3300
+#define MIN_LCD_V 2500
 
-  static void btnBacklight_event(lv_event_t *event) {
-    // TODO:
-    if (lcdVoltage > 2900) {
-      lcdVoltage = 2500;
-    } else {
-      lcdVoltage = 3100;
+  static void set_lcd_backlight_voltage(int pct) {
+    if (pct >= 0 && pct <= 100) {
+      int lcdVoltage = MIN_LCD_V + (MAX_LCD_V - MIN_LCD_V) * pct / 100;
+      M5.Axp.SetLDOVoltage(3, lcdVoltage);  // MIN_LCD_V to MAX_LCD_V
     }
-    M5.Axp.SetLDOVoltage(3, lcdVoltage);
   }
-
 
 #ifdef __cplusplus
 } /*extern "C"*/
