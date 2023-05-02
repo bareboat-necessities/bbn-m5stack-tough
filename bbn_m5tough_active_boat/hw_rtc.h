d3207c35f7f038b7a74f3b4dc57e22b8a286e18d
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-05-01 23:52:19 -0400

    Create hw_rtc.h

---
+++ bbn_m5tough_active_boat/hw_rtc.h
@@ -0,0 +1,15 @@
+#ifndef HW_RTC_H
+#define HW_RTC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  RTC_TimeTypeDef RTCtime;
+  RTC_DateTypeDef RTCdate;
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
