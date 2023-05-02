cf4b80b232e7ec830300d6c618d4202a304088f8
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-05-02 10:49:09 -0400

    Update ui_weather.h

--- bbn_m5tough_active_boat/ui_weather.h
+++ bbn_m5tough_active_boat/ui_weather.h
@@ -31,7 +31,8 @@ extern "C" {
 
     lv_obj_t *main_label = lv_label_create(parent);
     lv_obj_align(main_label, LV_ALIGN_CENTER, 0, -105);
-    lv_label_set_text(main_label, "SUN  " LV_SYMBOL_IMAGE);
+    lv_label_set_recolor(main_label, true);
+    lv_label_set_text(main_label, "SUN  #0080ff " LV_SYMBOL_IMAGE " #");
 
     weather_time_label = lv_label_create(parent);
     lv_obj_align(weather_time_label, LV_ALIGN_TOP_LEFT, 10, 30);
