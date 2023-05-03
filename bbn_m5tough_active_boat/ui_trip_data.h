e38680938b077fb78b57b0859135b70bde78675a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-05-03 01:58:57 -0400

    Update ui_trip_data.h

--- bbn_m5tough_active_boat/ui_trip_data.h
+++ bbn_m5tough_active_boat/ui_trip_data.h
@@ -100,6 +100,18 @@ extern "C" {
                             ? String(shipDataModel.navigation.heading_mag.deg, 0) + LV_SYMBOL_DEGREES
                             : String("--" LV_SYMBOL_DEGREES)))
                         .c_str());
+    lv_label_set_text(vmg_label,
+                      ("VMG (kn):                 "
+                       + (fresh(shipDataModel.navigation.course_rhumbline.next_point.velocity_made_good.age)
+                            ? String(shipDataModel.navigation.course_rhumbline.next_point.velocity_made_good.kn, 2)
+                            : String("--")))
+                        .c_str());
+    lv_label_set_text(rng_label,
+                      ("RNG (nm):                "
+                       + (fresh(shipDataModel.navigation.course_rhumbline.next_point.distance.age, 30000)
+                            ? String(shipDataModel.navigation.course_rhumbline.next_point.distance.m / NM_TO_METERS, 1)
+                            : String("--")))
+                        .c_str());
   }
 
   void init_tripDataScreen() {
