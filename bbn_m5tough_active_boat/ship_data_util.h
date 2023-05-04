8072dc0a93166d2733ab77ec4137dafc0d1fdf71
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-05-04 15:52:09 -0400

    Update ship_data_util.h

--- bbn_m5tough_active_boat/ship_data_util.h
+++ bbn_m5tough_active_boat/ship_data_util.h
@@ -5,20 +5,28 @@
 extern "C" {
 #endif
 
-  #define TWO_MINUTES 120000
-  #define LONG_EXPIRE_TO 172800000
+#define TWO_MINUTES 120000
+#define LONG_EXPIRE_TO 172800000
 
-  #define NM_TO_METERS 1852.0
+#define NM_TO_METERS 1852.0
 
   bool fresh(unsigned long age, unsigned long limit = 5000) {
     return millis() - age < limit && age != 0;
   }
 
   engine_t *lookup_engine(const char *engineID) {
+    int last = -1;
     for (int i = 0; i < MAX_ENGINES; i++) {
       if (strncmp(shipDataModel.propulsion.engines[i].engine_label, engineID, MAX_ENGINE_LBL_LENGTH) == 0) {
         return &shipDataModel.propulsion.engines[i];
       }
+      if (shipDataModel.propulsion.engines[i].engine_label[0] != 0) {
+        last++;
+      }
+    }
+    if ((last + 1) < MAX_ENGINES) {
+      strncpy((char *)(shipDataModel.propulsion.engines[last + 1].engine_label), engineID, MAX_ENGINE_LBL_LENGTH);
+      return &shipDataModel.propulsion.engines[last + 1];
     }
     return NULL;
   }
