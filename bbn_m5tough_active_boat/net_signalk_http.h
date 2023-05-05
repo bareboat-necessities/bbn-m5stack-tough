d9788d1d8de5205b8192d7591d8d380140df1c1c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-05-05 12:27:00 -0400

    Update net_signalk_http.h

--- bbn_m5tough_active_boat/net_signalk_http.h
+++ bbn_m5tough_active_boat/net_signalk_http.h
@@ -1,1 +1,30 @@
+#ifndef NET_SIGNALK_HTTP_H
+#define NET_SIGNALK_HTTP_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  void getVesselInfo() {
+    String api = "http://192.168.1.34:3000/signalk/v1/api/"; // TODO:
+    String resp = httpGETRequest((api + "vessels/self/design/beam/value/").c_str());
+    if (resp.length() > 0) {
+      shipDataModel.design.beam.m = resp.toFloat();
+      shipDataModel.design.beam.age = millis();
+    }
+    resp = httpGETRequest((api + "vessels/self/name/").c_str());
+    if (resp.length() > 0) {
+      strncpy(shipDataModel.vessel.name, resp.c_str(), sizeof(shipDataModel.vessel.name) - 1);
+    }
+    resp = httpGETRequest((api + "vessels/self/mmsi/").c_str());
+    if (resp.length() > 0) {
+      strncpy(shipDataModel.vessel.mmsi, resp.c_str(), sizeof(shipDataModel.vessel.mmsi) - 1);
+    }
+  }
+
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
