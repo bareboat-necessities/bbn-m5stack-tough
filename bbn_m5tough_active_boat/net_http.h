10a9416749a7480752666d639116b4e068edac68
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-05-05 10:47:57 -0400

    Create net_http.h

---
+++ bbn_m5tough_active_boat/net_http.h
@@ -0,0 +1,29 @@
+String httpGETRequest(const char* serverName) {
+  HTTPClient http;
+
+  // Your IP address with path or Domain name with URL path 
+  http.begin(serverName);
+
+  // If you need Node-RED/server authentication, insert user and password below
+  //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+
+
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+
+  String payload = "{}"; 
+
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
