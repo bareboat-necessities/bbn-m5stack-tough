fe4d3d0b1906230afff329060485efaa944be95c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-05-13 09:10:07 -0400

    Update ui_speed.h

--- bbn_m5tough_active_boat/ui_speed.h
+++ bbn_m5tough_active_boat/ui_speed.h
@@ -11,7 +11,8 @@ extern "C" {
   static lv_obj_t *sog_avg_label;
   static lv_obj_t *spd_label;
   static lv_obj_t *leeway_label;
-  static lv_obj_t *g_force_label;
+  static lv_obj_t *s_cogt_label;
+  static lv_obj_t *s_hdt_label;
 
   /**
    * A speed display 
@@ -24,39 +25,46 @@ extern "C" {
     lv_label_set_text(main_label, "SPEED  #0080ff " LV_SYMBOL_UPLOAD " #");
 
     sog_label = lv_label_create(parent);
-    lv_obj_align(sog_label, LV_ALIGN_TOP_LEFT, 10, 40);
+    lv_obj_align(sog_label, LV_ALIGN_TOP_LEFT, 10, 35);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(sog_label, &lv_font_montserrat_20, 0);
 #endif
     lv_label_set_text(sog_label, "SOG (kt):                       --");
 
     sog_avg_label = lv_label_create(parent);
-    lv_obj_align(sog_avg_label, LV_ALIGN_TOP_LEFT, 10, 80);
+    lv_obj_align(sog_avg_label, LV_ALIGN_TOP_LEFT, 10, 70);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(sog_avg_label, &lv_font_montserrat_20, 0);
 #endif
     lv_label_set_text(sog_avg_label, "SOG AVG (kt):             --");
 
     spd_label = lv_label_create(parent);
-    lv_obj_align(spd_label, LV_ALIGN_TOP_LEFT, 10, 120);
+    lv_obj_align(spd_label, LV_ALIGN_TOP_LEFT, 10, 105);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(spd_label, &lv_font_montserrat_20, 0);
 #endif
     lv_label_set_text(spd_label, "SPD (kt):                       --");
 
     leeway_label = lv_label_create(parent);
-    lv_obj_align(leeway_label, LV_ALIGN_TOP_LEFT, 10, 160);
+    lv_obj_align(leeway_label, LV_ALIGN_TOP_LEFT, 10, 140);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(leeway_label, &lv_font_montserrat_20, 0);
 #endif
     lv_label_set_text(leeway_label, "Leeway (est):              --" LV_SYMBOL_DEGREES);
 
-    g_force_label = lv_label_create(parent);
-    lv_obj_align(g_force_label, LV_ALIGN_TOP_LEFT, 10, 200);
+    s_cogt_label = lv_label_create(parent);
+    lv_obj_align(s_cogt_label, LV_ALIGN_TOP_LEFT, 10, 175);
 #if LV_FONT_MONTSERRAT_20
-    lv_obj_set_style_text_font(g_force_label, &lv_font_montserrat_20, 0);
+    lv_obj_set_style_text_font(s_cogt_label, &lv_font_montserrat_20, 0);
 #endif
-    lv_label_set_text(g_force_label, "G Force (% of g):        --");
+    lv_label_set_text(s_cogt_label, "COGT:                            --");
+
+    s_hdt_label = lv_label_create(parent);
+    lv_obj_align(s_hdt_label, LV_ALIGN_TOP_LEFT, 10, 210);
+#if LV_FONT_MONTSERRAT_20
+    lv_obj_set_style_text_font(s_hdt_label, &lv_font_montserrat_20, 0);
+#endif
+    lv_label_set_text(s_hdt_label, "HDT:                               --");
   }
 
   static void speed_update_cb() {
@@ -70,7 +78,16 @@ extern "C" {
                         .c_str());
     lv_label_set_text(leeway_label,
                       (("Leeway (est):              "
-                     + (fresh(shipDataModel.navigation.leeway.age) ? String(shipDataModel.navigation.leeway.deg, 1): String("--")))  + LV_SYMBOL_DEGREES)
+                        + (fresh(shipDataModel.navigation.leeway.age) ? String(shipDataModel.navigation.leeway.deg, 1) : String("--")))
+                       + LV_SYMBOL_DEGREES)
+                        .c_str());
+    lv_label_set_text(s_cogt_label,
+                      (String("COGT:                            ")
+                       + (fresh(shipDataModel.navigation.course_over_ground_true.age) ? String(shipDataModel.navigation.course_over_ground_true.deg, 1) + String(LV_SYMBOL_DEGREES) : String("--")))
+                        .c_str());
+    lv_label_set_text(s_hdt_label,
+                      (String("HDT:                               ")
+                      + (fresh(shipDataModel.navigation.heading_true.age) ? String(shipDataModel.navigation.heading_true.deg, 1) + String(LV_SYMBOL_DEGREES) : String("--")))
                         .c_str());
   }
 
