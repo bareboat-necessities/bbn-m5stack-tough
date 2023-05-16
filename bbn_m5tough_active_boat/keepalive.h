4ac63eddc6914bf9f0f482385490985bb326ec7c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-05-16 12:08:49 -0400

    Update keepalive.h

--- bbn_m5tough_active_boat/keepalive.h
+++ bbn_m5tough_active_boat/keepalive.h
@@ -18,6 +18,26 @@ extern "C" {
     wclient.setSocketOption(IPPROTO_TCP, TCP_KEEPINTVL, (const void *)&flags, sizeof(flags));
   }
 
+  static void disconnect_clients() {
+    if (skClient.c.connected()) {
+      skClient.c.stop();
+    }
+    if (pypClient.c.connected()) {
+      pypClient.c.stop();
+    }
+    if (nmea0183Client.c.connected()) {
+      nmea0183Client.c.stop();
+    }
+    if (mqttNetClient.connected()) {
+      mqttNetClient.stop();
+    }
+  }
+
+  static void ESP_restart() {
+    disconnect_clients();
+    ESP.restart();
+  }
+
 #ifdef __cplusplus
 } /*extern "C"*/
 #endif
