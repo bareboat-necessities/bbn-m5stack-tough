0af35d67b8b0a5693ba5be9028dd8b1b65633865
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-05-16 21:08:23 -0400

    Update ui_depth.h

--- bbn_m5tough_active_boat/ui_depth.h
+++ bbn_m5tough_active_boat/ui_depth.h
@@ -61,27 +61,27 @@ extern "C" {
 
   static void depth_update_cb() {
     lv_label_set_text(dbt_label,
-                      ("DBT (ft):          "
-                       + (fresh(shipDataModel.environment.depth.below_transducer.age)
+                      (String("DBT (ft):          ")
+                       += (fresh(shipDataModel.environment.depth.below_transducer.age)
                             ? String(shipDataModel.environment.depth.below_transducer.m * _GPS_FEET_PER_METER, 1)
                             : String("--")))
                         .c_str());
     lv_label_set_text(dbk_label,
-                      ("DBK (ft):          "
-                       + (fresh(shipDataModel.environment.depth.below_keel.age)
+                      (String("DBK (ft):          ")
+                       += (fresh(shipDataModel.environment.depth.below_keel.age)
                             ? String(shipDataModel.environment.depth.below_keel.m * _GPS_FEET_PER_METER, 1)
                             : String("--")))
                         .c_str());
     lv_label_set_text(dbs_label,
-                      ("DBS (ft):          "
-                       + (fresh(shipDataModel.environment.depth.below_surface.age)
+                      (String("DBS (ft):          ")
+                       += (fresh(shipDataModel.environment.depth.below_surface.age)
                             ? String(shipDataModel.environment.depth.below_surface.m * _GPS_FEET_PER_METER, 1)
                             : String("--")))
                         .c_str());
     lv_label_set_text(d_heel_label,
-                      ("Heel:                 "
-                       + (fresh(shipDataModel.navigation.attitude.heel.age)
-                            ? String(shipDataModel.navigation.attitude.heel.deg, 1) + String(LV_SYMBOL_DEGREES)
+                      (String("Heel:                 ")
+                       += (fresh(shipDataModel.navigation.attitude.heel.age)
+                            ? String(shipDataModel.navigation.attitude.heel.deg, 1) += String(LV_SYMBOL_DEGREES)
                             : String("--")))
                            .c_str());
   }
