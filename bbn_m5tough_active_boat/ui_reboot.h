ff06852f13b8c85ca21ce2f57966ee102e452737
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-05-19 16:40:00 -0400

    Update ui_reboot.h

--- bbn_m5tough_active_boat/ui_reboot.h
+++ bbn_m5tough_active_boat/ui_reboot.h
@@ -25,14 +25,14 @@ extern "C" {
     lv_obj_t *btn1 = lv_btn_create(parent);
     lv_obj_t *label1 = lv_label_create(btn1);
     lv_obj_align(btn1, LV_ALIGN_CENTER, 0, 20);
-    lv_label_set_text(label1, "Reboot");
+    lv_label_set_text_static(label1, "Reboot");
     lv_obj_center(label1);
     lv_obj_add_event_cb(btn1, btnReboot_event, LV_EVENT_PRESSED, NULL);
 
     lv_obj_t *btn2 = lv_btn_create(parent);
     lv_obj_t *label2 = lv_label_create(btn2);
     lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 80);
-    lv_label_set_text(label2, LV_SYMBOL_POWER);
+    lv_label_set_text_static(label2, LV_SYMBOL_POWER);
     lv_obj_center(label2);
     lv_obj_add_event_cb(btn2, btnPowerOff_event, LV_EVENT_LONG_PRESSED, NULL);
     lv_obj_set_style_bg_color(btn2, lv_palette_main(LV_PALETTE_RED), LV_PART_MAIN);
@@ -40,7 +40,7 @@ extern "C" {
     lv_obj_t *btn3 = lv_btn_create(parent);
     lv_obj_t *label3 = lv_label_create(btn3);
     lv_obj_align(btn3, LV_ALIGN_CENTER, 0, -40);
-    lv_label_set_text(label3, "Reset Wi-Fi");
+    lv_label_set_text_static(label3, "Reset Wi-Fi");
     lv_obj_center(label3);
     lv_obj_add_event_cb(btn3, btnResetWiFiSettings_event, LV_EVENT_LONG_PRESSED, NULL);
     lv_obj_set_style_bg_color(btn3, lv_palette_main(LV_PALETTE_RED), LV_PART_MAIN);
