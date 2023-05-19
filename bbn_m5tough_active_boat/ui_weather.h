44f862be02fd401e540494102625a8be6143bf2c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-05-19 16:07:45 -0400

    Update ui_weather.h

--- bbn_m5tough_active_boat/ui_weather.h
+++ bbn_m5tough_active_boat/ui_weather.h
@@ -32,56 +32,56 @@ extern "C" {
     lv_obj_t *main_label = lv_label_create(parent);
     lv_obj_align(main_label, LV_ALIGN_CENTER, 0, -105);
     lv_label_set_recolor(main_label, true);
-    lv_label_set_text(main_label, "SUN  #0080ff " LV_SYMBOL_IMAGE " #");
+    lv_label_set_text_static(main_label, "SUN  #0080ff " LV_SYMBOL_IMAGE " #");
 
     weather_time_label = lv_label_create(parent);
     lv_obj_align(weather_time_label, LV_ALIGN_TOP_LEFT, 10, 30);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(weather_time_label, &lv_font_montserrat_20, 0);
 #endif
-    lv_label_set_text(weather_time_label, "UTC Time:");
+    lv_label_set_text_static(weather_time_label, "UTC Time:");
 
     weather_twilight_begin_label = lv_label_create(parent);
     lv_obj_align(weather_twilight_begin_label, LV_ALIGN_TOP_LEFT, 10, 60);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(weather_twilight_begin_label, &lv_font_montserrat_20, 0);
 #endif
-    lv_label_set_text(weather_twilight_begin_label, "Twilight Start:");
+    lv_label_set_text_static(weather_twilight_begin_label, "Twilight Start:");
 
     weather_sunrise_label = lv_label_create(parent);
     lv_obj_align(weather_sunrise_label, LV_ALIGN_TOP_LEFT, 10, 90);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(weather_sunrise_label, &lv_font_montserrat_20, 0);
 #endif
-    lv_label_set_text(weather_sunrise_label, "Sunrise:");
+    lv_label_set_text_static(weather_sunrise_label, "Sunrise:");
 
     weather_daylight_label = lv_label_create(parent);
     lv_obj_align(weather_daylight_label, LV_ALIGN_TOP_LEFT, 10, 120);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(weather_daylight_label, &lv_font_montserrat_20, 0);
 #endif
-    lv_label_set_text(weather_daylight_label, "Day Duration:");
+    lv_label_set_text_static(weather_daylight_label, "Day Duration:");
 
     weather_sunset_label = lv_label_create(parent);
     lv_obj_align(weather_sunset_label, LV_ALIGN_TOP_LEFT, 10, 150);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(weather_sunset_label, &lv_font_montserrat_20, 0);
 #endif
-    lv_label_set_text(weather_sunset_label, "Sunset:");
+    lv_label_set_text_static(weather_sunset_label, "Sunset:");
 
     weather_twilight_label = lv_label_create(parent);
     lv_obj_align(weather_twilight_label, LV_ALIGN_TOP_LEFT, 10, 180);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(weather_twilight_label, &lv_font_montserrat_20, 0);
 #endif
-    lv_label_set_text(weather_twilight_label, "Twilight Duration:");
+    lv_label_set_text_static(weather_twilight_label, "Twilight Duration:");
 
     weather_twilight_end_label = lv_label_create(parent);
     lv_obj_align(weather_twilight_end_label, LV_ALIGN_TOP_LEFT, 10, 210);
 #if LV_FONT_MONTSERRAT_20
     lv_obj_set_style_text_font(weather_twilight_end_label, &lv_font_montserrat_20, 0);
 #endif
-    lv_label_set_text(weather_twilight_end_label, "Twilight End:");
+    lv_label_set_text_static(weather_twilight_end_label, "Twilight End:");
   }
 
   static String hour_format(float hr) {
