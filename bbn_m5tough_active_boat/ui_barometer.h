42217c32eac4f299ef24468303a0617abfe88f37
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-01-04 09:15:41 -0500

    Update ui_barometer.h

--- bbn_m5tough_active_boat/ui_barometer.h
+++ bbn_m5tough_active_boat/ui_barometer.h
@@ -13,6 +13,8 @@ extern "C" {
   static lv_obj_t *pressure_label;
   static lv_obj_t *temperature_label;
   static lv_obj_t *temperature_bar;
+  static lv_obj_t *humidity_label;
+  static lv_obj_t *humidity_bar;
 
   static void set_pressure_value(void *indic, int32_t v) {
     lv_meter_set_indicator_value(barometer_display, (lv_meter_indicator_t *)indic, v);
@@ -20,6 +22,10 @@ extern "C" {
 
   static void set_temperature_value(int32_t v) {
     lv_bar_set_value(temperature_bar, v, LV_ANIM_OFF);
+  }
+
+  static void set_humidity_value(int32_t v) {
+    lv_bar_set_value(humidity_bar, v, LV_ANIM_OFF);
   }
 
   /**
@@ -75,10 +81,7 @@ extern "C" {
     static lv_style_t style_indic;
 
     lv_style_init(&style_indic);
-    //lv_style_set_bg_opa(&style_indic, LV_OPA_COVER);
     lv_style_set_bg_color(&style_indic, lv_palette_lighten(LV_PALETTE_ORANGE, 2));
-    //lv_style_set_bg_grad_color(&style_indic, lv_palette_main(LV_PALETTE_BLUE));
-    //lv_style_set_bg_grad_dir(&style_indic, LV_GRAD_DIR_VER);
 
     temperature_bar = lv_bar_create(parent);
     lv_obj_add_style(temperature_bar, &style_indic, LV_PART_INDICATOR);
@@ -93,21 +96,49 @@ extern "C" {
     lv_obj_set_style_text_font(temperature_label, &lv_font_montserrat_20, 0);
 #endif
     lv_label_set_text_static(temperature_label, ((String("-- ") += LV_SYMBOL_DEGREES) += "C").c_str());
+
+    /*Humidity*/
+    static lv_style_t style_indic2;
+
+    lv_style_init(&style_indic2);
+    lv_style_set_bg_color(&style_indic2, lv_palette_lighten(LV_PALETTE_BLUE, 1));
+
+    humidity_bar = lv_bar_create(parent);
+    lv_obj_add_style(humidity_bar, &style_indic2, LV_PART_INDICATOR);
+    lv_obj_set_style_bg_color(humidity_bar, lv_palette_lighten(LV_PALETTE_GREY, 3), LV_PART_MAIN);
+    lv_obj_set_size(humidity_bar, 20, 145);
+    lv_obj_align(humidity_bar, LV_ALIGN_CENTER, -125, -10);
+    lv_bar_set_range(humidity_bar, 0, 100);
+
+    humidity_label = lv_label_create(parent);
+    lv_obj_align(humidity_label, LV_ALIGN_CENTER, -125, 85);
+#if LV_FONT_MONTSERRAT_20
+    lv_obj_set_style_text_font(humidity_label, &lv_font_montserrat_20, 0);
+#endif
+    lv_label_set_text_static(humidity_label, "-- %");
+
   }
 
   static void pressure_update_cb() {
     lv_label_set_text(pressure_label,
-                      ((fresh(shipDataModel.environment.air_outside.pressure.age) ? 
+                      ((fresh(shipDataModel.environment.air_outside.pressure.age, 1800000) ? 
                         String(shipDataModel.environment.air_outside.pressure.hPa, 0) += "\n  hPa" : String("    --\n  hPa")))
                         .c_str());
 
-    set_pressure_value(indic_barometer, fresh(shipDataModel.environment.air_outside.pressure.age) ? shipDataModel.environment.air_outside.pressure.hPa : 0);
+    set_pressure_value(indic_barometer, fresh(shipDataModel.environment.air_outside.pressure.age, 1800000) ? shipDataModel.environment.air_outside.pressure.hPa : 0);
 
-    set_temperature_value(fresh(shipDataModel.environment.air_outside.temp_deg_C.age) ? shipDataModel.environment.air_outside.temp_deg_C.deg_C : 0);
+    set_temperature_value(fresh(shipDataModel.environment.air_outside.temp_deg_C.age, 1800000) ? shipDataModel.environment.air_outside.temp_deg_C.deg_C : 0);
 
     lv_label_set_text(temperature_label,
-                      ((fresh(shipDataModel.environment.air_outside.temp_deg_C.age) ? 
+                      ((fresh(shipDataModel.environment.air_outside.temp_deg_C.age, 1800000) ? 
                         ((String(shipDataModel.environment.air_outside.temp_deg_C.deg_C, 0) += " ") += LV_SYMBOL_DEGREES) += "C" : (String("-- ") += LV_SYMBOL_DEGREES) += "C"))
+                        .c_str());
+
+    set_humidity_value(fresh(shipDataModel.environment.air_outside.humidity_pct.age, 1800000) ? shipDataModel.environment.air_outside.humidity_pct.pct : 0);
+
+    lv_label_set_text(humidity_label,
+                      (fresh(shipDataModel.environment.air_outside.humidity_pct.age, 1800000) ? 
+                        (String(shipDataModel.environment.air_outside.humidity_pct.pct, 0) += " %") : String("-- %"))
                         .c_str());
   }
 
